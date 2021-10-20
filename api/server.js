4adf9228f834a18bee65bf159ef35f73ceeeacb8
David VanDusen <github@dvw3.com>
2021-10-20 09:17:50 -0700

    #2 Add and configure body-parser, express-session, and helmet

--- api/server.js
+++ api/server.js
@@ -1,6 +1,28 @@
+const bodyParser = require('body-parser');
 const express = require('express');
+const helmet = require('helmet');
+const session = require('express-session');
 
 const app = express();
+
+app.use(helmet());
+
+app.use(bodyParser.json());
+
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET || 'default session secret',
+    name: 'session_id',
+    resave: true,
+    saveUninitialized: true,
+    sameSite: true,
+    secure: true,
+  })
+);
+
+app.get('/', (req, res) => {
+  res.json({});
+});
 
 const port = process.env.PORT || 8491;
 
