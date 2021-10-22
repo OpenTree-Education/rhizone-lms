d9af9091112d6e3f76b258c797bc57f77d1f47a6
David VanDusen <github@dvw3.com>
2021-10-21 20:10:06 -0700

    #8 Send auth error as json

--- api/src/authMiddleware.ts
+++ api/src/authMiddleware.ts
@@ -1,9 +1,15 @@
 import { Request, Response, NextFunction } from 'express';
 
+import { errorEnvelope } from './responseEnvelope';
+
 export const loggedIn = (req: Request, res: Response, next: NextFunction) => {
   if (req.session.principalId) {
     next();
     return;
   }
-  res.sendStatus(401);
+  res
+    .status(401)
+    .json(
+      errorEnvelope('This session is not authenticated. Sign in to continue.')
+    );
 };
