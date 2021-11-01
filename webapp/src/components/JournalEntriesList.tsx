0dc68c0f71f70226c300511892042c692ee0ec0a
Veronica Leung <tungtungleung233@hotmail.com>
2021-11-01 17:10:22 -0400

    add journalEntry date/time creation to state

--- webapp/src/components/JournalEntriesList.tsx
+++ webapp/src/components/JournalEntriesList.tsx
@@ -11,11 +11,16 @@ const JournalEntriesList = ({ journalEntries }: JournalEntriesListProps) => (
   <>
     <h2>Your previous entries</h2>
     <Stack spacing={2}>
-      {journalEntries.map(({ id, raw_text: rawText }) => (
-        <Card key={id}>
-          <CardContent>{rawText}</CardContent>
-        </Card>
-      ))}
+      {journalEntries.map(({ id, raw_text: rawText, created_at: createdAt }) => {
+        
+        return (  
+          <Card key={id}>
+            <CardContent>{createdAt}</CardContent>
+            <CardContent>{new Intl.DateTimeFormat([], { dateStyle: 'full', timeStyle: 'full' }).format(parseInt(createdAt))}</CardContent>
+            <CardContent>{rawText}</CardContent>
+          </Card>
+        )
+      })}
     </Stack>
   </>
 );
