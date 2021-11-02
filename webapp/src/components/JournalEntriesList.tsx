6600c43ee512192d64c6afafea5f7ac21d95b196
Adrian Zaragoza <azaragoza.lupian@gmail.com>
2021-11-02 13:24:06 -0700

    #57 pass date and time of entry to JournalEntry Card

--- webapp/src/components/JournalEntriesList.tsx
+++ webapp/src/components/JournalEntriesList.tsx
@@ -11,11 +11,25 @@ const JournalEntriesList = ({ journalEntries }: JournalEntriesListProps) => (
   <>
     <h2>Your previous entries</h2>
     <Stack spacing={2}>
-      {journalEntries.map(({ id, raw_text: rawText }) => (
-        <Card key={id}>
-          <CardContent>{rawText}</CardContent>
-        </Card>
-      ))}
+      {journalEntries.map(({ id, raw_text: rawText, created_at: createdAt }) => {
+        const date = new Date(createdAt);
+
+        return (  
+          <Card key={id}>
+            <CardContent>
+              {new Intl.DateTimeFormat([], 
+              { 
+                year: 'numeric', 
+                month: 'long', 
+                day: 'numeric', 
+                hour: 'numeric', 
+                minute: 'numeric', 
+              }).format(date)}
+            </CardContent>
+            <CardContent>{rawText}</CardContent>
+          </Card>
+        )
+      })}
     </Stack>
   </>
 );
