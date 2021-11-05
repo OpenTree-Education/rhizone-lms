248576203877d902ce77ab5fdb5bd9c9d2aa6d44
Veronica Leung <tungtungleung233@hotmail.com>
2021-11-05 13:08:23 -0400

    #57 address PR comments and changes
    - changed null value to optional argument in formatDateTime function
    - renamed variable 'localeArr' to 'locales'
    - reworded formatDateTime tests

--- webapp/src/helpers/dateTime.test.ts
+++ webapp/src/helpers/dateTime.test.ts
@@ -1,25 +1,26 @@
 import { formatDateTime } from './dateTime';
 
-describe('formatDateTime function', () => {
-  describe('Timezones', () => {
-    it('should always be UTC', () => {
-      expect(new Date().getTimezoneOffset()).toBe(0);
-    });
+describe('Timezones', () => {
+  it('should always be UTC', () => {
+    expect(new Date().getTimezoneOffset()).toBe(0);
   });
+});
 
-  it('should return a string containing journal entry date and time in en-CA format', () => {
+describe('formatDateTime function', () => {
+
+  it('should return a string containing date and time in en-CA format', () => {
     const result = formatDateTime('2021-11-04T10:00:00.000Z', 'en-CA');
 
     expect(result).toBe('November 4, 2021, 10:00 a.m.');
   });
 
-  it('should return a string containing journal entry date and time in en-US format', () => {
+  it('should return a string containing date and time in en-US format', () => {
     const result = formatDateTime('2021-11-04T10:00:00.000Z', 'en-US');
 
     expect(result).toBe('November 4, 2021, 10:00 AM');
   });
 
-  it('should return a string containing journal entry date and time in fr-CA format', () => {
+  it('should return a string containing date and time in fr-CA format', () => {
     const result = formatDateTime('2021-11-04T10:00:00.000Z', 'fr-CA');
 
     expect(result).toBe('4 novembre 2021, 10 h 00');
--- webapp/src/helpers/dateTime.ts
+++ webapp/src/helpers/dateTime.ts
@@ -1,12 +1,12 @@
-export const formatDateTime = (date: string, locale: string | null = null) => {
+export const formatDateTime = (date: string, locale?: string) => {
   const dateObj = new Date(date);
-  let localeArr: [] | string = [];
+  let locales: [] | string = [];
 
   if (locale) {
-    localeArr = locale;
+    locales = locale;
   }
 
-  return new Intl.DateTimeFormat(localeArr, {
+  return new Intl.DateTimeFormat(locales, {
     year: 'numeric',
     month: 'long',
     day: 'numeric',
