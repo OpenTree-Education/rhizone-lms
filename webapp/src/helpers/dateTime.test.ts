d9d4a94ec894f2e90ff9438597516997218c267a
Veronica Leung <tungtungleung233@hotmail.com>
2021-11-05 13:17:31 -0400

    #57 restructure describe blocks for dateTime tests

--- webapp/src/helpers/dateTime.test.ts
+++ webapp/src/helpers/dateTime.test.ts
@@ -1,28 +1,23 @@
 import { formatDateTime } from './dateTime';
 
-describe('Timezones', () => {
-  it('should always be UTC', () => {
-    expect(new Date().getTimezoneOffset()).toBe(0);
-  });
-});
+describe('dateTime', () => {
+  describe('formatDateTime function', () => {
+    it('should return a string containing date and time in en-CA format', () => {
+      const result = formatDateTime('2021-11-04T10:00:00.000Z', 'en-CA');
 
-describe('formatDateTime function', () => {
+      expect(result).toBe('November 4, 2021, 10:00 a.m.');
+    });
 
-  it('should return a string containing date and time in en-CA format', () => {
-    const result = formatDateTime('2021-11-04T10:00:00.000Z', 'en-CA');
+    it('should return a string containing date and time in en-US format', () => {
+      const result = formatDateTime('2021-11-04T10:00:00.000Z', 'en-US');
 
-    expect(result).toBe('November 4, 2021, 10:00 a.m.');
-  });
+      expect(result).toBe('November 4, 2021, 10:00 AM');
+    });
 
-  it('should return a string containing date and time in en-US format', () => {
-    const result = formatDateTime('2021-11-04T10:00:00.000Z', 'en-US');
+    it('should return a string containing date and time in fr-CA format', () => {
+      const result = formatDateTime('2021-11-04T10:00:00.000Z', 'fr-CA');
 
-    expect(result).toBe('November 4, 2021, 10:00 AM');
-  });
-
-  it('should return a string containing date and time in fr-CA format', () => {
-    const result = formatDateTime('2021-11-04T10:00:00.000Z', 'fr-CA');
-
-    expect(result).toBe('4 novembre 2021, 10 h 00');
+      expect(result).toBe('4 novembre 2021, 10 h 00');
+    });
   });
 });
