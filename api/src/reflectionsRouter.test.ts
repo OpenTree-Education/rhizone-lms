c5bf66ea1339c61094b09594c024ff6450b76616
Akmal <akmalanvaroff@gmail.com>
2021-11-11 16:34:34 -0500

    #61 Move tracker.on inside loginExistingUser helper

--- api/src/reflectionsRouter.test.ts
+++ api/src/reflectionsRouter.test.ts
@@ -20,30 +20,33 @@ describe('reflectionsRouter', () => {
     });
 
     it('should create a reflection, a journal entry, associate the journal entry to the reflection, and associate both with the principal, and respond with an envelope containing the id of the newly created reflection.', done => {
-      tracker.on('query', ({ bindings, sql, transacting, response }) => {
-        if (sql === 'insert into `reflections` (`principal_id`) values (?)') {
-          expect(bindings).toEqual([process.env.MOCK_PRINCIPAL_ID]);
-          expect(transacting).toEqual(true);
-          response([process.env.MOCK_PRINCIPAL_ID]);
-        } else if (
-          sql ===
-          'insert into `journal_entries` (`principal_id`, `raw_text`, `reflection_id`) values (?, ?, ?)'
-        ) {
-          expect(bindings).toEqual([
-            process.env.MOCK_PRINCIPAL_ID,
-            'Hello! I feel good today',
-            process.env.MOCK_REFLECTION_ID,
-          ]);
-          expect(transacting).toEqual(true);
-          response([process.env.MOCK_REFLECTION_ID]);
-        } else if (sql === 'BEGIN;') {
-          response(null);
-        } else if (sql === 'COMMIT;') {
-          response(null);
-        }
-      });
-
       loginExistingUser(appAgent => {
+        tracker.on('query', ({ bindings, sql, transacting, response }) => {
+          if (sql === 'insert into `reflections` (`principal_id`) values (?)') {
+            expect(bindings).toEqual([process.env.MOCK_PRINCIPAL_ID]);
+            expect(transacting).toEqual(true);
+            response([process.env.MOCK_PRINCIPAL_ID]);
+          } else if (
+            sql ===
+            'insert into `journal_entries` (`principal_id`, `raw_text`) values (?, ?)'
+          ) {
+            expect(bindings).toEqual([
+              process.env.MOCK_PRINCIPAL_ID,
+              'Hello! I feel good today',
+              process.env.MOCK_REFLECTION_ID,
+            ]);
+            expect(transacting).toEqual(true);
+            response([process.env.MOCK_REFLECTION_ID]);
+          } else if (sql === 'BEGIN;') {
+            response(null);
+          } else if (sql === 'COMMIT;') {
+            response(null);
+          } else {
+            done(
+              `Didn't match any known SQL statement cases. Recieved SQL statement: ${sql}`
+            );
+          }
+        });
         appAgent
           .post('/reflections')
           .send({ raw_text: 'Hello! I feel good today' })
