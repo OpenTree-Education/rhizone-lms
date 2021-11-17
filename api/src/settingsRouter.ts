f80b38b9038ba0470adfbe2c710ef72045878d4d
Akmal <akmalanvaroff@gmail.com>
2021-11-17 16:57:32 -0500

    #105 create settings router

---
+++ api/src/settingsRouter.ts
@@ -0,0 +1,25 @@
+import { Router } from 'express';
+
+import { BadRequestError } from './httpErrors';
+import { collectionEnvelope, itemEnvelope } from './responseEnvelope';
+import db from './db';
+
+const settingsRouter = Router();
+
+settingsRouter.get('/:category', async (req, res, next) => {
+  const categoryName = req.params.category;
+  try {
+    const settings = await db('settings')
+      .select('property', 'content')
+      .where({ category: categoryName });
+    const countAlias = 'total_count';
+    const totalCounts = await db('settings')
+      .count({ [countAlias]: '*' })
+      .where({ category: categoryName });
+    res.json(collectionEnvelope(settings, totalCounts[0][countAlias]));
+  } catch (error) {
+    next(error);
+    return;
+  }
+});
+export default settingsRouter;
