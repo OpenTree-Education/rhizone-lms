d29aba9092db3d2da6ba3a296813f93a24147af4
David VanDusen <github@dvw3.com>
2021-11-29 13:30:38 -0800

    #106 Implement GET /questionnaires/:id and create questionnaires service

--- api/src/questionnairesRouter.ts
+++ api/src/questionnairesRouter.ts
@@ -1,9 +1,34 @@
 import { Router } from 'express';
 
+import { BadRequestError, NotFoundError } from './httpErrors';
+import { findQuestionnaire } from './questionnairesService';
+import { itemEnvelope } from './responseEnvelope';
+
 const questionnairesRouter = Router();
 
-questionnairesRouter.get('/:id', (req, res, next) => {
-  next('Not implemented');
+questionnairesRouter.get('/:id', async (req, res, next) => {
+  const { id } = req.params;
+  const questionnaireId = Number(id);
+  if (!Number.isInteger(questionnaireId) || questionnaireId < 1) {
+    next(new BadRequestError(`"${id}" is not a valid questionnaire id.`));
+    return;
+  }
+  let questionnaire;
+  try {
+    questionnaire = await findQuestionnaire(questionnaireId);
+  } catch (err) {
+    next(err);
+    return;
+  }
+  if (!questionnaire) {
+    next(
+      new NotFoundError(
+        `A questionnaire with the id "${id}" could not be found.`
+      )
+    );
+    return;
+  }
+  res.json(itemEnvelope(questionnaire));
 });
 
 export default questionnairesRouter;
