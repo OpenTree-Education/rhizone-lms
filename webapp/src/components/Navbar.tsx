f8ad0f3bf6693078fde9bad946cc7de0d19d7d06
Veronica Leung <tungtungleung233@hotmail.com>
2021-12-06 14:22:40 -0500

    #169 change navbar to a class component

--- webapp/src/components/Navbar.tsx
+++ webapp/src/components/Navbar.tsx
@@ -1,35 +1,44 @@
 import { Button, Grid } from '@mui/material';
-import React from 'react';
+import React, {Component} from 'react';
 
 interface NavbarProps {
   loggedIn: boolean | null;
 }
 
-const Navbar = ({ loggedIn }: NavbarProps) => (
-  <Grid alignItems="center" container>
-    <Grid item xs>
-      <h1>Rhizone</h1>
-    </Grid>
-    <Grid item xs="auto">
-      {loggedIn === false && (
-        <Button
-          component="a"
-          href={`${process.env.REACT_APP_API_ORIGIN}/auth/github/login`}
-          variant="contained"
-        >
-          Sign In
-        </Button>
-      )}
-      {loggedIn === true && (
-        <Button
-          component="a"
-          href={`${process.env.REACT_APP_API_ORIGIN}/auth/logout`}
-        >
-          Sign Out
-        </Button>
-      )}
-    </Grid>
-  </Grid>
-);
+class Navbar extends Component<NavbarProps> {
+  constructor(props: NavbarProps) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <Grid alignItems="center" container>
+        <Grid item xs>
+          <h1>Rhizone</h1>
+        </Grid>
+        <Grid item xs="auto">
+          {this.props.loggedIn === false && (
+            <Button
+              component="a"
+              href={`${process.env.REACT_APP_API_ORIGIN}/auth/github/login`}
+              variant="contained"
+            >
+              Sign In
+            </Button>
+          )}
+          {this.props.loggedIn === true && (
+            <Button
+              component="a"
+              href={`${process.env.REACT_APP_API_ORIGIN}/auth/logout`}
+            >
+              Sign Out
+            </Button>
+          )}
+        </Grid>
+      </Grid>
+    )
+  }
+}
+
 
 export default Navbar;
