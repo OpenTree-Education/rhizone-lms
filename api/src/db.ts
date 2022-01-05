8eef1d93224101a53db8d23a0443bf485f7072d4
David VanDusen <github@dvw3.com>
2022-01-04 16:41:27 -0800

    #229 Configure app to run with Docker Compose in dev and update docs

--- api/src/db.ts
+++ api/src/db.ts
@@ -6,7 +6,6 @@ const db = knex({
     database: process.env.MYSQL_DATABASE,
     host: process.env.MYSQL_HOST,
     password: process.env.MYSQL_PASSWORD,
-    port: Number(process.env.MYSQL_PORT),
     user: process.env.MYSQL_USER,
   },
 });
--- api/src/server.ts
+++ api/src/server.ts
@@ -1,10 +1,7 @@
-import dotenv from 'dotenv';
-dotenv.config();
-
 import app from './app';
 
 app.listen(app.get('port'), app.get('host'), () => {
   const host = app.get('host');
   const port = app.get('port');
-  console.log(`api listening on port ${host}:${port}`);
+  console.log(`api listening on ${host}:${port}`);
 });
