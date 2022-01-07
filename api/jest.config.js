a644cef2729ae581b7de998b1a2e9162505ab558
David VanDusen <github@dvw3.com>
2022-01-06 22:23:40 -0800

    #237 Move api jest config into package.json

--- api/jest.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
-module.exports = {
-  clearMocks: true,
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  testMatch: ['<rootDir>/src/**/*.test.ts'],
-  setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
-};
