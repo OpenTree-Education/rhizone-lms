434f8d608b32678fe77de59b6b5c4c1c0754ed54
Eileen <xue.eileen@gmail.com>
2022-01-24 15:44:43 -0500

    #271 Create model_competencies table to store a user's tracked competencies

--- db/migration/V012__create_competencies_tables.sql
+++ db/migration/V012__create_competencies_tables.sql
@@ -10,5 +10,12 @@ CREATE TABLE competencies (
 );
 
 CREATE TABLE model_competencies (
-
+  id BIGINT NOT NULL AUTO_INCREMENT,
+  principal_id BIGINT NOT NULL,
+  competency_id BIGINT NOT NULL,
+  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (id),
+  FOREIGN KEY (principal_id) REFERENCES principals(id),
+  FOREIGN KEY (competency_id) REFERENCES competencies(id)
 );
