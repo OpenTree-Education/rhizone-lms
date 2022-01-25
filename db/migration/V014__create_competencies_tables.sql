b8145407e946ab9c18f2972981c82d0923fc8e9d
Eileen <xue.eileen@gmail.com>
2022-01-25 15:00:06 -0500

    #271 Update constraints and data types for competencies tables

--- db/migration/V014__create_competencies_tables.sql
+++ db/migration/V014__create_competencies_tables.sql
@@ -1,11 +1,12 @@
 CREATE TABLE competencies (
   id BIGINT NOT NULL AUTO_INCREMENT,
-  label TEXT NOT NULL,
-  description TEXT NOT NULL,
+  label VARCHAR(255) NOT NULL CHECK(label <> ''),
+  description TEXT NOT NULL CHECK(description <> ''),
   principal_id BIGINT NOT NULL,
   created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (id),
+  INDEX competencies_principal_id (principal_id),
   FOREIGN KEY (principal_id) REFERENCES principals(id)
 );
 
@@ -16,6 +17,9 @@ CREATE TABLE model_competencies (
   created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (id),
+  INDEX model_competencies_principal_id (principal_id),
   FOREIGN KEY (principal_id) REFERENCES principals(id),
-  FOREIGN KEY (competency_id) REFERENCES competencies(id)
+  INDEX model_competencies_competency_id (competency_id),
+  FOREIGN KEY (competency_id) REFERENCES competencies(id),
+  UNIQUE (principal_id, competency_id)
 );
