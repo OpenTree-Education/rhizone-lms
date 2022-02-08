8e811347a74a00672a6d420f1a35f1e45b7b0f1e
Eileen <xue.eileen@gmail.com>
2022-02-08 14:39:20 -0500

    #275 Add competency form to competencies page to allow users to add new competencies to the database

--- webapp/src/components/CompetenciesPage.tsx
+++ webapp/src/components/CompetenciesPage.tsx
@@ -1,13 +1,17 @@
-import { Container } from '@mui/material';
-import React from 'react';
+import { Box, Container, Grid } from '@mui/material';
+import React, { useState } from 'react';
 
 import Competency from './Competency';
-import { Competency as APICompetency } from '../types/api';
+import CreateCompetencyForm from './CreateCompetencyForm';
+import { EntityId, Competency as APICompetency } from '../types/api';
 import useApiData from '../helpers/useApiData';
 
 const CompetenciesPage = () => {
+  const [newlyCreatedCompetencyIds, setNewlyCreatedCompetencyIds] = useState<
+    EntityId[]
+  >([]);
   const { data: competencies, error } = useApiData<APICompetency[]>({
-    deps: [],
+    deps: [newlyCreatedCompetencyIds],
     path: `/competencies`,
     sendCredentials: true,
   });
@@ -19,10 +23,24 @@ const CompetenciesPage = () => {
   }
   return (
     <Container fixed>
-      {competencies.length === 0 && <p>There are no competencies.</p>}
-      {competencies.map(({ id, label, description }) => (
-        <Competency key={id} description={description} label={label} />
-      ))}
+      <h1>Competencies</h1>
+      <Grid container justifyContent="center">
+        <Grid item xs={12} md={8}>
+          <Box>
+            <CreateCompetencyForm
+              onCompetencyCreated={id =>
+                setNewlyCreatedCompetencyIds([...newlyCreatedCompetencyIds, id])
+              }
+            />
+          </Box>
+          <Box my={6}>
+            {competencies.length === 0 && <p>There are no competencies.</p>}
+            {competencies.map(({ id, label, description }) => (
+              <Competency key={id} description={description} label={label} />
+            ))}
+          </Box>
+        </Grid>
+      </Grid>
     </Container>
   );
 };
