cf5034ca3f92151c8e5f4adf2aeca9012f17154c
David VanDusen <github@dvw3.com>
2022-02-23 13:32:50 -0800

    #178 Replace logical or with forced String conversion of socket config

--- webapp/src/helpers/useSocket.ts
+++ webapp/src/helpers/useSocket.ts
@@ -2,6 +2,6 @@ import { io, Socket } from 'socket.io-client';
 
 let socket: Socket;
 const useSocket = () =>
-  socket || (socket = io(process.env.REACT_APP_API_ORIGIN || {}));
+  socket || (socket = io(String(process.env.REACT_APP_API_ORIGIN)));
 
 export default useSocket;
