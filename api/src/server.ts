c4c8d6ae30c61913b8fa054eb27b923dd0237ce3
David VanDusen <github@dvw3.com>
2022-03-04 12:40:45 -0800

    #177 Document @ts-ignore usage in server.ts for socket.request.session

--- api/src/server.ts
+++ api/src/server.ts
@@ -56,6 +56,9 @@ const start = async () => {
 
   io.on('connection', socket => {
     socket.on('meeting:join', async meetingId => {
+      // There was a type conflict when declaring types for socket.request that
+      // caused the build to break.
+      // See: https://github.com/OpenTree-Education/rhizone-lms/pull/328
       // @ts-ignore
       const { principalId } = socket.request.session;
       if (await participantExists(meetingId, principalId)) {
