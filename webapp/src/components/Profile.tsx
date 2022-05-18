fb847ce37e937d82bd73f9574694880ef4beb8db
nazaninrezaei88@gmail.com <nazaninrezaei88@gmail.com>
2022-05-18 15:08:07 -0400

    #348 create buttons on profile page to redirect user to other features like Journal and Competency

--- webapp/src/components/Profile.tsx
+++ webapp/src/components/Profile.tsx
@@ -1,7 +1,33 @@
 import React from 'react';
+import Button from '@mui/material/Button';
+import Stack from '@mui/material/Stack';
+import { useNavigate } from 'react-router-dom';
 
 const Profile = () => {
-  return <div>Profile</div>;
+  let navigate = useNavigate();
+  const routeChangeCompetency = () => {
+    const path = `/competencies`;
+    navigate(path);
+  };
+
+  const routeChangeJournal = () => {
+    const path = `/`;
+    navigate(path);
+  };
+
+  return (
+    <div>
+      Profile
+      <Stack spacing={2} direction="row">
+        <Button variant="outlined" onClick={routeChangeJournal}>
+          Journal
+        </Button>
+        <Button variant="outlined" onClick={routeChangeCompetency}>
+          Competencies
+        </Button>
+      </Stack>
+    </div>
+  );
 };
 
 export default Profile;
