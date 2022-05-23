d7ea0cd7b690953952244f1e53437f2b3b54be2c
Spencer W <4006420+seidior@users.noreply.github.com>
2022-05-23 13:34:09 -0700

    #355 Example Database Structure
    We worked together to create an example database structure of how a user's social links could be defined without having to hard-code network info into their own columns.

--- db/migration/V015__add_user_data.sql
+++ db/migration/V015__add_user_data.sql
@@ -1,2 +1,25 @@
-ALTER TABLE github_users ADD COLUMN first_name VARCHAR(255);
-ALTER TABLE github_users ADD COLUMN last_name VARCHAR(255);
+ALTER TABLE github_users ADD COLUMN full_name VARCHAR(255);
+
+CREATE TABLE social_networks (
+    `id` bigint NOT NULL AUTO_INCREMENT,
+    `network_name` VARCHAR(255) NOT NULL,
+    `base_url` VARCHAR(255) NOT NULL,
+    `icon_name` VARCHAR(255) NOT NULL,
+    PRIMARY KEY (`id`),
+    UNIQUE KEY `unique_network_name` (`network_name`)
+) ENGINE=InnoDB charset=utf8mb4;
+
+CREATE TABLE principal_social (
+    `id` bigint NOT NULL AUTO_INCREMENT,
+    `principal_id` bigint,
+    `network_id` bigint,
+    `data` VARCHAR(255),
+    PRIMARY KEY (`id`),
+    CONSTRAINT `principal_social_1` FOREIGN KEY (`principal_id`) REFERENCES `principals` (`id`),
+    CONSTRAINT `principal_social_2` FOREIGN KEY (`network_id`) REFERENCES `social_networks` (`id`)
+) ENGINE=InnoDB charset=utf8mb4;
+
+INSERT INTO `social_networks` (`id`, `network_name`, `base_url`)
+VALUES
+    (1, 'github', 'github.com', 'github.svg'),
+    (2, 'linkedin', 'linkedin.com/in', 'linkedin.svg');
