a8dda6815dd3891d809eca4c8d32e8e145266522
Spencer W <4006420+seidior@users.noreply.github.com>
2022-05-24 13:09:26 -0700

    #355 Add additional social networks.

---
+++ db/migration/V016__add_more_social_networks.sql
@@ -0,0 +1,6 @@
+INSERT INTO `social_networks` (`network_name`, `base_url`, `icon_name`)
+VALUES
+    ('twitter', 'twitter.com', 'twitter.svg'),
+    ('reddit', 'reddit.com/user', 'reddit.svg'),
+    ('website', '', 'website.svg'),
+    ('dribbble', 'dribbble.com/user', 'dribbble.svg');
