bf458925c0ddf9c595b32ab00aeffb8f8dade0a6
Shea Cronin <shealcronin@gmail.com>
2022-05-26 15:14:38 -0400

    #355 Test adding fullname to GitHub user

--- api/src/services/githubUsersService.ts
+++ api/src/services/githubUsersService.ts
@@ -1,5 +1,5 @@
 import db from './db';
-import {IUserData} from "../middleware/authRouter";
+import { IUserData } from '../middleware/authRouter';
 
 export const findGithubUserByGithubId = async (githubId: number) => {
   const [githubUser] = await db('github_users')
@@ -9,11 +9,13 @@ export const findGithubUserByGithubId = async (githubId: number) => {
 };
 
 export const createGithubUser = async (githubUserData: IUserData) => {
+  console.log('Creating GitHub user...');
   const githubUser: {
     github_id: number;
     id?: number;
     principal_id?: number;
-  } = { github_id: githubUserData.id };
+    full_name?: string;
+  } = { github_id: githubUserData.id, full_name: githubUserData.fullname };
   await db.transaction(async trx => {
     const insertedPrincipalIds = await trx('principals').insert({
       entity_type: 'user',
