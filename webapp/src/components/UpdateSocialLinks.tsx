f028f275d58cbf60d050a55b0eb72846f092fb83
Antonina Chichikalova <tonia.chichikalova@gmail.com>
2022-06-01 11:10:48 -0400

    #347 Add icons to dropdown

--- webapp/src/components/UpdateSocialLinks.tsx
+++ webapp/src/components/UpdateSocialLinks.tsx
@@ -1,13 +1,15 @@
+import React, { useState } from 'react';
 import {
   FormControl,
   Grid,
-  InputLabel,
   MenuItem,
   Select,
   SelectChangeEvent,
   TextField,
 } from '@mui/material';
-import React, { useState } from 'react';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import LanguageIcon from '@mui/icons-material/Language';
+import TwitterIcon from '@mui/icons-material/Twitter';
 
 interface SocialLinksProps {
   socialName: string;
@@ -32,12 +34,11 @@ const UpdateSocialLinks = ({ socialName, networkData }: SocialLinksProps) => {
     >
       <FormControl
         variant="standard"
-        sx={{ m: 1, minWidth: 100 }}
-        disabled={socialName == 'github'}
+        sx={{
+          m: 1,
+          minWidth: 150,
+        }}
       >
-        {/* <InputLabel id="demo-simple-select-standard-label">
-          Social Networks
-        </InputLabel> */}
         <Select
           labelId="demo-simple-select-standard-label"
           id="demo-simple-select-standard"
@@ -45,17 +46,40 @@ const UpdateSocialLinks = ({ socialName, networkData }: SocialLinksProps) => {
           onChange={handleChange}
           label="Social Networks"
         >
-          <MenuItem value="twitter">Twitter</MenuItem>
-          <MenuItem value="linkedIn">LinkedIn</MenuItem>
-          <MenuItem value="portfolio">Portfolio</MenuItem>
+          <MenuItem value="twitter">
+            <TwitterIcon
+              color="primary"
+              sx={{
+                justifyContent: 'flex-start',
+                alignItems: 'flex-end',
+                mr: 1,
+              }}
+            />
+            Twitter
+          </MenuItem>
+          <MenuItem value="linkedIn">
+            <LinkedInIcon
+              color="primary"
+              sx={{
+                justifyContent: 'flex-start',
+                mr: 1,
+              }}
+            />
+            LinkedIn
+          </MenuItem>
+          <MenuItem value="portfolio">
+            <LanguageIcon
+              color="primary"
+              sx={{
+                justifyContent: 'flex-start',
+                mr: 1,
+              }}
+            />
+            Portfolio
+          </MenuItem>
         </Select>
       </FormControl>
-      <TextField
-        type="text"
-        value={networkData}
-        name={socialNetworks}
-        variant="standard"
-      />
+      <TextField type="text" name={socialNetworks} variant="standard" />
     </Grid>
   );
 };
