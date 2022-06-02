8fcccec789856889afe23c65e078e3203181f41f
Spencer Wohlers <4006420+seidior@users.noreply.github.com>
2022-06-02 14:58:32 -0700

    #355: Separate out service from router
    My last commit was a bit dirty, combining the service with the router. This separates it out and follows the conventions of the other files in these directories.

--- api/src/middleware/socialNetworksRouter.ts
+++ api/src/middleware/socialNetworksRouter.ts
@@ -1,16 +1,19 @@
 import { Router } from 'express';
-import db from '../services/db';
-import { ISocialNetwork } from '../models/user_models';
 import { collectionEnvelope } from './responseEnvelope';
+import { listSocialNetworks } from '../services/socialNetworksService';
+import { ISocialNetwork } from '../models/user_models';
 
 const socialNetworksRouter = Router();
 
 socialNetworksRouter.get('/', async (_req, res, next) => {
   try {
-    const social_networks_query = db('social_networks').select<ISocialNetwork[]>("*");
-    social_networks_query.then((social_networks_list: ISocialNetwork[]) => {
-      res.json(collectionEnvelope(social_networks_list, social_networks_list.length));
-    });
+    await listSocialNetworks().then(
+      (social_networks_list: ISocialNetwork[]) => {
+        res.json(
+          collectionEnvelope(social_networks_list, social_networks_list.length)
+        );
+      }
+    );
   } catch (err) {
     console.error('err', err);
     next(err);
