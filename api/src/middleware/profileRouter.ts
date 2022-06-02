cb537b0dcade575157161b33db48a16fd8a65396
Spencer Wohlers <4006420+seidior@users.noreply.github.com>
2022-06-02 13:50:44 -0700

    #355: Rebase fixes
    Bring back Denize and James' changes from after the merge that were lost when rebasing.

--- api/src/middleware/profileRouter.ts
+++ api/src/middleware/profileRouter.ts
@@ -6,7 +6,7 @@ const profileRouter = Router();
 
 profileRouter.get('/', async (_req, _res, next) => {
   try {
-    throw new Error("I need to be passed a principal ID.");
+    throw new Error('I need to be passed a principal ID.');
   } catch (err) {
     console.error('err', err);
     next(err);
@@ -15,12 +15,14 @@ profileRouter.get('/', async (_req, _res, next) => {
 });
 
 profileRouter.get('/:principal_id', async (req, res, next) => {
-  const {principal_id} = req.params;
+  const { principal_id } = req.params;
   const principalId = parseInt(principal_id);
 
   try {
     if (principal_id == null) {
-      throw new Error("I was not passed a principal_id for which to show profile data.");
+      throw new Error(
+        'I was not passed a principal_id for which to show profile data.'
+      );
     }
 
     // TODO: need to check here if we're getting our own profile or someone else's!
@@ -28,25 +30,26 @@ profileRouter.get('/:principal_id', async (req, res, next) => {
     const user_profile_data = await getUserProfileData(principalId);
 
     if (user_profile_data == null) {
-      throw new Error(`I couldn't find a principal ID matching ${principal_id}`);
+      throw new Error(
+        `I couldn't find a principal ID matching ${principal_id}`
+      );
     }
 
     res.json(collectionEnvelope(Array(user_profile_data), 1));
   } catch (err) {
-    console.error('err', err)
+    console.error('err', err);
     next(err);
     return;
   }
-
 });
 
 profileRouter.post('/:principal_id', async (req, res, next) => {
-  const {principal_id} = req.params;
+  const { principal_id } = req.params;
   const principalId = parseInt(principal_id);
   const current_principal = req.session.principalId;
 
   // Check to make sure we're logged into the same principal_id for which we got the request
-  
+
   // Parse the submitted information into usable information
 
   // Call each individual function to save the modified data into the database
--- api/src/services/githubUsersService.ts
+++ api/src/services/githubUsersService.ts
@@ -5,14 +5,23 @@ import { IGitHubUser } from '../models/user_models';
  * This function searches the `github_users` table for entries matching a
  * GitHub user ID. If it finds a row, it returns it, formatted as an
  * IGitHubUser object. If it doesn't, it returns null.
- * 
+ *
  * @param githubId (integer) GitHub user ID
  * @returns IGitHubUser corresponding to row in table or null
  */
-export const findGithubUserByGithubId = async (githubId: number): Promise<IGitHubUser | null> => {
+export const findGithubUserByGithubId = async (
+  githubId: number
+): Promise<IGitHubUser | null> => {
   const github_users: IGitHubUser[] = await db('github_users')
-    .select<IGitHubUser[]>('github_id', 'username', 'full_name', 'bio', 'avatar_url', 'principal_id')
-    .where({github_id: githubId})
+    .select<IGitHubUser[]>(
+      'github_id',
+      'username',
+      'full_name',
+      'bio',
+      'avatar_url',
+      'principal_id'
+    )
+    .where({ github_id: githubId })
     .limit(1);
 
   return github_users.length > 0 ? github_users[0] : null;
@@ -22,14 +31,23 @@ export const findGithubUserByGithubId = async (githubId: number): Promise<IGitHu
  * This function searches the `github_users` table for entries matching a
  * principal ID. If it finds matches, it returns them, formatted as
  * IGitHubUser objects. If it doesn't, it returns null.
- * 
+ *
  * @param principalId (integer) principal ID number
  * @returns IGitHubUser[] corresponding to rows in table or null
  */
- export const findGithubUsersByPrincipalId = async (principalId: number): Promise<IGitHubUser[] | null> => {
+export const findGithubUsersByPrincipalId = async (
+  principalId: number
+): Promise<IGitHubUser[] | null> => {
   const github_users: IGitHubUser[] = await db('github_users')
-    .select<IGitHubUser[]>('github_id', 'username', 'full_name', 'bio', 'avatar_url', 'principal_id')
-    .where({principal_id: principalId})
+    .select<IGitHubUser[]>(
+      'github_id',
+      'username',
+      'full_name',
+      'bio',
+      'avatar_url',
+      'principal_id'
+    )
+    .where({ principal_id: principalId })
     .limit(1);
 
   return github_users.length > 0 ? github_users : null;
@@ -37,11 +55,13 @@ export const findGithubUserByGithubId = async (githubId: number): Promise<IGitHu
 
 /**
  * Creates an entry in the `github_users` table with the information passed.
- * 
+ *
  * @param githubUser (IGitHubUser) object containing data to insert into table
  * @returns same object as parameter but with the principal_id instantiated
  */
-export const createGithubUser = async (githubUser: IGitHubUser): Promise<IGitHubUser> => {
+export const createGithubUser = async (
+  githubUser: IGitHubUser
+): Promise<IGitHubUser> => {
   await db.transaction(async trx => {
     // TODO: we need to pre-populate the principal table with the bio and
     // full_name fields from GitHub but allow the user to override.
@@ -56,7 +76,7 @@ export const createGithubUser = async (githubUser: IGitHubUser): Promise<IGitHub
       principal_id: githubUser.principal_id,
       network_id: 1, // GitHub id in social_networks table
       data: githubUser.username,
-      public: true
+      public: true,
     });
   });
   return githubUser;
