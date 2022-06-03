59f2f44740520216ccef3d605257b8718bb98f14
SiriusLL <Sirius.love.light@gmail.com>
2022-06-02 20:30:14 -0600

    #356 Connect api data to profile page

--- webapp/src/types/api.d.ts
+++ webapp/src/types/api.d.ts
@@ -76,3 +76,27 @@ export interface SessionData extends Entity {
 export interface Settings extends Entity {
   default_questionnaire_id: EntityId;
 }
+interface GithubAccounts extends Entity {
+  github_id: number;
+  username: string;
+  full_name: string;
+  bio: string;
+  avatar_url: string;
+  principal_id: EntityId;
+}
+interface SocialProfiles extends Entity {
+network_name: string;
+user_name: string;
+profile_url: string;
+public: boolean;
+
+}
+export interface Profile extends Entity {
+  id: number;
+  full_name: string;
+  email_address: string;
+  bio: string;
+  github_accounts: GithubAccounts[];
+  social_profiles: SocialProfiles[];
+  profile_url: string;
+}
