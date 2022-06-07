f1bbec0421bef80da862c2b20ac5ef98b172da40
Spencer Wohlers <4006420+seidior@users.noreply.github.com>
2022-06-07 16:05:38 -0700

    #356: Also mock console.error
    We're mocking and catching console.log but we also should be mocking console.error.

--- api/src/jest.setup.ts
+++ api/src/jest.setup.ts
@@ -3,3 +3,8 @@ console.log = (...args) => {
   originalConsoleLog(...args);
   throw new Error('Unexpected call to console.log.');
 };
+const originalConsoleError = console.error;
+console.error = (...args) => {
+  originalConsoleError(...args);
+  throw new Error('Unexpected call to console.error.');
+};
