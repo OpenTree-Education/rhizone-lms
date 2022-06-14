b85a11305dad78b6caad2587adf0632e0caaef46
Shea Cronin <shealcronin@gmail.com>
2022-06-14 14:21:12 -0400

    Add migration to add competency categories table

---
+++ db/migration/V019__create_competency_categories_table.sql
@@ -0,0 +1,16 @@
+CREATE TABLE categories (
+  id BIGINT NOT NULL AUTO_INCREMENT,
+  label VARCHAR(255) NOT NULL,
+  description TEXT NOT NULL,
+  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (id)
+);
+
+ALTER TABLE competencies 
+    ADD COLUMN category_id BIGINT
+    ADD INDEX competencies_category_id (category_id)
+    ADD FOREIGN KEY (category_id) REFERENCES categories(id)
+    ADD COLUMN subcategory_id BIGINT
+    ADD INDEX competencies_subcategory_id (subcategory_id)
+    ADD FOREIGN KEY (subcategory_id) REFERENCES categories(id)
