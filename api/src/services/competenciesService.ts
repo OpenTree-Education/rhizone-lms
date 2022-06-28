a9caee0c2384b84412848477ea62ca0ef8c517c3
nazaninrezaei88@gmail.com <nazaninrezaei88@gmail.com>
2022-06-28 14:14:29 -0400

    remove limit and offset features

--- api/src/services/competenciesService.ts
+++ api/src/services/competenciesService.ts
@@ -63,12 +63,10 @@ export const getAllCompetenciesByCategory = async (categoryId: number) => {
   return competencies;
 };
 
-export const listCategories = async (limit: number, offset: number) => {
+export const listCategories = async () => {
   const categories = await db('categories')
     .select('id', 'label', 'description', 'image_url')
     .orderBy('label', 'asc')
     .orderBy('id', 'asc')
-    .limit(limit)
-    .offset(offset);
   return categories;
 };
