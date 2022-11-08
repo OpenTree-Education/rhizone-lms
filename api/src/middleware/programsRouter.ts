d8be3e83eebb005e6187f99190110a262f487cea
Cami Anderson <camidantas@icloud.com>
2022-11-08 16:41:05 -0500

    #415 Add get function to programsRouter

--- api/src/middleware/programsRouter.ts
+++ api/src/middleware/programsRouter.ts
@@ -3,7 +3,24 @@ import { Router } from 'express';
 import { BadRequestError, NotFoundError, ValidationError } from './httpErrors';
 import { collectionEnvelope, itemEnvelope } from './responseEnvelope';
 import { findProgram } from '../services/programsService';
+import { parsePaginationParams } from './paginationParamsMiddleware';
 
 const programsRouter = Router();
+
+programsRouter.get('/', parsePaginationParams(), async (req, res, next) => {
+  const { limit, offset } = req.pagination;
+  let programs;
+  let programsCount;
+  try {
+    [programs, programsCount] = await Promise.all([
+      listPrograms(limit, offset),
+      countPrograms(),
+    ]);
+  } catch (err) {
+    next(err);
+    return;
+  }
+  res.json(collectionEnvelope(programs, programsCount));
+});
 
 export default programsRouter;
