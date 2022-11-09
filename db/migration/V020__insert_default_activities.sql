50059247a69b889ddaa12af6b9a618dbd7e914c8
Phoebe <phyuhala@gmail.com>
2022-11-08 17:28:16 -0800

    #413 WIP: new migration file

--- db/migration/V020__insert_default_activities.sql
+++ db/migration/V020__insert_default_activities.sql
@@ -1,14 +1,5 @@
-INSERT INTO questionnaires () VALUES
-  ();
-SET @questionnaire_id = LAST_INSERT_ID();
-INSERT INTO settings (property, content, category) VALUES
-  ('default_questionnaire_id', CAST(@questionnaire_id AS CHAR), 'webapp');
-INSERT INTO prompts (label, query_text, sort_order, questionnaire_id) VALUES
-  ('Outlook', 'How are you feeling about your current endeavours?', 1, @questionnaire_id);
-SET @prompt_id = LAST_INSERT_ID();
-INSERT INTO options (label, numeric_value, sort_order, prompt_id) VALUES
-  ('Very discouraged', -2, 1, @prompt_id),
-  ('A little discouraged', -1, 2, @prompt_id),
-  ('I don’t know', 0, 3, @prompt_id),
-  ('A little hopeful', 1, 4, @prompt_id),
-  ('Very hopeful', 2, 5, @prompt_id);
+INSERT INTO curriculums (principal_id, title) VALUES
+  (1, 'Professional Mentorship Program');
+INSERT INTO activities (title, description_text, curriculum_week, curriculum_day, start_time, end_time, duration, activity_type_id, curriculum_id) VALUES
+  ('Set up local copy of repository', 'Clone the repository and follow the instructions to set it up on your development machine.', 1, 1, NULL, NULL, NULL, 2, 1),  
+  ('Morning standup', '', 1, 1, '10:00:00', '11:00:00', 60, 3, 1);
