a3393e55cb2410d3be520b62baa5d980b4c7027c
Chengxu <chengxuzhang123@gmail.com>
2022-11-09 13:24:22 -0800

    #415 Add findProgram content in programsService

--- api/src/middleware/programsRouter.ts
+++ api/src/middleware/programsRouter.ts
@@ -2,19 +2,20 @@ import { Router } from 'express';
 
 import { BadRequestError, NotFoundError, ValidationError } from './httpErrors';
 import { collectionEnvelope, itemEnvelope } from './responseEnvelope';
-import { findProgram } from '../services/programsService';
+import { countProgram,listPrograms } from '../services/programsService';
 import { parsePaginationParams } from './paginationParamsMiddleware';
 
 const programsRouter = Router();
 
 programsRouter.get('/', parsePaginationParams(), async (req, res, next) => {
+  const { principalId } = req.session;
   const { limit, offset } = req.pagination;
   let programs;
   let programsCount;
   try {
     [programs, programsCount] = await Promise.all([
-      listPrograms(limit, offset),
-      countPrograms(),
+      listPrograms(principalId, limit, offset),
+      countProgram(principalId),
     ]);
   } catch (err) {
     next(err);
--- api/src/services/programsService.ts
+++ api/src/services/programsService.ts
@@ -1,7 +1,17 @@
 import db from './db';
 
-export const findProgram = async () => {
-  const countAlias = 'total_count';
-  const [count] = await db('programs').count({ [countAlias]: '*' });
-  return count[countAlias];
+export const countProgram = async (principalId: number) => {
+    const countAlias = 'total_count';
+    const [count] = await db('programs')
+      .count({ [countAlias]: '*' })
+      .where({ principal_id: principalId });
+    // console.log(count[countAlias]);
+    return count[countAlias];
 };
+
+export const findProgram = async (principalId: number) => {
+    const thisProgram = await db('programs')
+        .select('id', 'title')
+        .where({principal_id: principalId});
+    return thisProgram;
+}
