064c7315ed1e7732f36f9462914898a017794fde
Cami Anderson <camidantas@icloud.com>
2022-11-10 20:10:24 -0500

    #415 Add functionality to programRouter and programsService

--- api/src/middleware/programsRouter.ts
+++ api/src/middleware/programsRouter.ts
@@ -2,26 +2,31 @@ import { Router } from 'express';
 
 import { BadRequestError, NotFoundError, ValidationError } from './httpErrors';
 import { collectionEnvelope, itemEnvelope } from './responseEnvelope';
-import { countProgram,listPrograms } from '../services/programsService';
+import { listPrograms, listCurriculumActivities } from '../services/programsService';
 import { parsePaginationParams } from './paginationParamsMiddleware';
 
 const programsRouter = Router();
 
 programsRouter.get('/', parsePaginationParams(), async (req, res, next) => {
-  const { principalId } = req.session;
   const { limit, offset } = req.pagination;
-  let programs;
-  let programsCount;
+  let allPrograms;
   try {
-    [programs, programsCount] = await Promise.all([
-      listPrograms(principalId, limit, offset),
-      countProgram(principalId),
-    ]);
+    allPrograms = await listPrograms();
+    const programActivities = await listCurriculumActivities(allPrograms[0].id);
+    allPrograms[0].activities = programActivities;
+    // allPrograms.forEach(async (program) => {
+    //   const programActivities = await listCurriculumActivities(program.id);
+    //   program.activities = programActivities;
+    // });
   } catch (err) {
     next(err);
     return;
   }
-  res.json(collectionEnvelope(programs, programsCount));
+  if (allPrograms && allPrograms.length) {
+    res.json(collectionEnvelope(allPrograms, allPrograms.length));
+  } else {
+    res.json(collectionEnvelope(null, 0));
+  }
 });
 
 export default programsRouter;
--- api/src/services/programsService.ts
+++ api/src/services/programsService.ts
@@ -1,17 +1,25 @@
 import db from './db';
 
-export const countProgram = async (principalId: number) => {
-    const countAlias = 'total_count';
-    const [count] = await db('programs')
-      .count({ [countAlias]: '*' })
-      .where({ principal_id: principalId });
-    // console.log(count[countAlias]);
-    return count[countAlias];
-};
+export const listPrograms = async () => {
+    const foundPrograms = await db('programs');
+    return foundPrograms ?? [];
+}
 
-export const findProgram = async (principalId: number) => {
-    const thisProgram = await db('programs')
-        .select('id', 'title')
-        .where({principal_id: principalId});
-    return thisProgram;
+export const findProgram = async (programId: number) => {
+    return await db('programs').where('id', programId) ?? [];
+}
+
+export const listCurriculumActivities = async (curriculumId?: number) => {
+    let foundActivities;
+
+    if (curriculumId) {
+        foundActivities = await db('activities').select('activities.title', 'activities.description_text', 'activities.curriculum_week', 'activities.curriculum_day', 'activities.start_time', 'activities.end_time', 'activities.duration', 'activity_types.title as activity_type').leftJoin('activity_types', 'activities.activity_type_id', 'activity_types.id').where({curriculum_id: curriculumId});
+    } else {
+        foundActivities = await db('activities');
+    }
+    return foundActivities ?? [];
+}
+
+export const findActivity = async (activityId: number) => {
+    return await db('activities').where('id', activityId) ?? [];
 }
