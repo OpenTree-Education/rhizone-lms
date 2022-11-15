dfcb24e18f8937d302d290f43e2db2d4c2ee4831
Spencer W <4006420+seidior@users.noreply.github.com>
2022-11-15 13:32:40 -0800

    #415 Example data for tests
    Add comment with example database data so that we can start to develop example data in the tests.

--- api/src/services/__tests__/programsService.ts
+++ api/src/services/__tests__/programsService.ts
@@ -9,6 +9,44 @@ import {
 } from '../programsService';
 import { mockQuery } from '../mockDb';
 
+/*
+  Assume this is the data that will be in the database before your test is run.
+  In a text document stored on one of your pair programming partner's computers,
+  write down what you think the return value should be from your function given
+  the following data is the current state of the database.
+
+  Do *not* reference any of the example data that appear in the V020 migration.
+  As far as these tests are concerned, the database *only* contains the data
+  you see below.
+
+  If your function takes a parameter, you'll also need to specify the parameter
+  value, and the return value of the function should match the data when that
+  specific parameter is passed.
+
+  HINT: use the types defined in models.d.ts to conform the return value to the
+  expected structure.
+
+  # ACTIVITIES:
+  id, title, description_text, curriculum_week, curriculum_day, start_time, end_time, duration, activity_type_id, curriculum_id, created_at, updated_at
+  1, 'Morning Standup', '', 1, 1, '10:00:00', '11:00:00', 60, 3, 1, "2022-11-15 01:23:45", "2022-11-15 01:23:45"
+  2, 'Self-introduction', 'Get to know each other.', 1, 1, '11:10:00', '12:00:00', 50, 1, 1, "2022-11-15 01:23:45", "2022-11-15 01:23:45"
+  3, 'Self-assessment', '', 1, 2, NULL, NULL, NULL, 2, 1, "2022-11-15 01:23:45", "2022-11-15 01:23:45"
+  4, 'Morning Standup', '', 1, 1, '10:00:00', '11:00:00', 60, 3, 2, "2022-11-15 01:23:45", "2022-11-15 01:23:45"
+  5, 'Self-introduction', 'Get to know each other.', 1, 1, '11:10:00', '12:00:00', 50, 1, 2, "2022-11-15 01:23:45", "2022-11-15 01:23:45"
+  6, 'Self-assessment', '', 1, 2, NULL, NULL, NULL, 2, 2, "2022-11-15 01:23:45", "2022-11-15 01:23:45"
+
+  # PROGRAMS:
+  id, principal_id, curriculum_id, title, start_date, end_date, created_at, updated_at
+  1, 2, 1, 'Cohort 4', '2022-10-24', '2022-12-16', "2022-11-15 01:23:45", "2022-11-15 01:23:45"
+  2, 2, 2, 'Cohort 5', '2022-10-24', '2022-12-16', "2022-11-15 01:23:45", "2022-11-15 01:23:45"
+  3, 2, 1, 'Cohort 6', '2023-01-02', '2023-02-24', "2022-11-15 01:23:45", "2022-11-15 01:23:45"
+
+  # CURRICULUMS:
+  id, principal_id, title, created_at, updated_at
+  1, 2, 'Professional Mentorship Program', "2022-11-15 01:23:45", "2022-11-15 01:23:45"
+  2, 2, 'Honors Professional Mentorship Program', "2022-11-15 01:23:45", "2022-11-15 01:23:45"
+*/
+
 describe('programsService', () => {
   describe('listPrograms', () => {
     it('should list all available programs', async () => {});
