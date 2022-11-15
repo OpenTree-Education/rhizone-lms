c42e0036474a6cd2f04db14e90554e3e18f60f8a
Spencer W <4006420+seidior@users.noreply.github.com>
2022-11-15 15:13:03 -0800

    #415 Rearrange models.d.ts
    Rearrange the `models.d.ts` file to better match the order of the V019 migration.

--- api/src/models.d.ts
+++ api/src/models.d.ts
@@ -1,26 +1,24 @@
 export interface Program {
   id: number;
-  principal_id: number;
-  curriculum_id: number;
   title: string;
   start_date: string;
   end_date: string;
+  principal_id: number;
+  curriculum_id: number;
   created_at: string;
   updated_at: string;
 }
 
 export interface Curriculum {
   id: number;
+  title: string;
   principal_id: number;
   created_at: string;
   updated_at: string;
-  title: string;
 }
 
 export interface CurriculumActivity {
   id: number;
-  created_at: string;
-  updated_at: string;
   title: string;
   description_text: string;
   curriculum_week: number;
@@ -30,16 +28,19 @@ export interface CurriculumActivity {
   duration: number | null;
   activity_type_id: number;
   curriculum_id: number;
+  created_at: string;
+  updated_at: string;
 }
 
 export interface ProgramActivity {
+  title: string;
+  description_text: string;
   program_id: number;
+  curriculum_activity_id: number;
   activity_type: string;
   start_time?: Date;
   end_time?: Date;
   duration?: number;
-  title: string;
-  description_text: string;
 }
 
 export interface ProgramWithActivities extends Program {
