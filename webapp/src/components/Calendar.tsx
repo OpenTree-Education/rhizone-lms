a7a2a65e2dc4fda1e46f90ee412a043b0b9c4192
Prabhj33t <105026997+Prabhj33t@users.noreply.github.com>
2022-11-18 03:14:48 -0800

    416 added logo as a link

---
+++ webapp/src/components/Calendar.tsx
@@ -0,0 +1,11 @@
+import { Box, Typography } from '@mui/material';
+import { Link } from 'react-router-dom';
+import React from 'react';
+
+const Calendar = () => (
+  <Box >
+    <Link to=''>go to the calendar</Link>
+  </Box>
+);
+
+export default Calendar;
