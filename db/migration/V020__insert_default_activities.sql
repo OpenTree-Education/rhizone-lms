2f1d0b785aab56398f32b770c68b7af0ae338cc5
Spencer W <4006420+seidior@users.noreply.github.com>
2022-11-18 15:23:47 -0800

    #415 Switch to IANA Time Zones in db
    When specifying a time zone in the database, let’s use IANA time zones. This means that it’ll handle DST automatically.

--- db/migration/V020__insert_default_activities.sql
+++ db/migration/V020__insert_default_activities.sql
@@ -6,7 +6,7 @@ INSERT INTO curriculums (principal_id, title) VALUES
 SET @curriculumId = LAST_INSERT_ID();
 
 INSERT INTO programs (title, start_date, end_date, time_zone, principal_id, curriculum_id) VALUES 
-  ('Cohort 4', '2022-10-24', '2022-12-16', '-08:00', @principalId, @curriculumId);
+  ('Cohort 4', '2022-10-24', '2022-12-16', 'America/Los_Angeles', @principalId, @curriculumId);
 
 INSERT INTO activities (title, description_text, curriculum_week, curriculum_day, start_time, end_time, duration, activity_type_id, curriculum_id) VALUES
   -- Week 1: Day 1
