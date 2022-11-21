ed55a483db1d0960e770bec8aebdb20139b4dc8c
Spencer W <4006420+seidior@users.noreply.github.com>
2022-11-21 10:48:19 -0800

    #427 Set up the ticket
    State the goals for this ticket.

--- webapp/src/components/CalendarPage.tsx
+++ webapp/src/components/CalendarPage.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 
 const CalendarPage = () => {
+  // The goal in this ticket is to wire the appropriate functions to ensure
+  // programsList gets filled in with API data so we can render it.
+  let programsList;
   return <div>Calendar Page</div>;
 };
 
