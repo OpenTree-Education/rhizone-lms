f3f8b2d068f333460750f39c00d144795d361145
Phoebe <phyuhala@gmail.com>
2022-11-21 14:19:11 -0800

    #427 Fetch program data from API and create program and activity components

--- webapp/src/components/CalendarPage.tsx
+++ webapp/src/components/CalendarPage.tsx
@@ -1,10 +1,51 @@
+import { Container } from '@mui/material';
 import React from 'react';
+import useApiData from '../helpers/useApiData';
+
+import { ProgramWithActivities } from '../types/api';
+import Program from './Program';
 
 const CalendarPage = () => {
-  // The goal in this ticket is to wire the appropriate functions to ensure
-  // programsList gets filled in with API data so we can render it.
-  let programsList;
-  return <div>Calendar Page</div>;
+  const {
+    data: programs,
+    error,
+    isLoading,
+  } = useApiData<ProgramWithActivities[]>({
+    deps: [],
+    path: '/programs',
+    sendCredentials: true,
+  });
+  if (error) {
+    return <p>There was an error loading the programs.</p>;
+  }
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (!programs) {
+    return null;
+  }
+
+  return (
+    <Container fixed>
+      <h2>Programs</h2>
+      {programs.map(program => {
+        return (
+          <Program
+            key={program.id}
+            id={program.id}
+            title={program.title}
+            startDate={program.start_date}
+            endDate={program.end_date}
+            timeZone={program.time_zone}
+            curriculumId={program.curriculum_id}
+            activities={program.activities}
+          />
+        );
+      })}
+    </Container>
+  );
 };
 
 export default CalendarPage;
