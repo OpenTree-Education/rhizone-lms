c64ca2c1728fe9fff7afb0c1f9e85d08cb30656c
Cami Anderson <camidantas@icloud.com>
2022-11-22 16:12:42 -0500

    #428 Add API to CalendarPage

--- webapp/src/components/CalendarPage.tsx
+++ webapp/src/components/CalendarPage.tsx
@@ -1,38 +1,19 @@
+import {
+  Calendar,
+  luxonLocalizer,
+  Event as RBCEvent,
+} from 'react-big-calendar';
 import { Container } from '@mui/material';
-import {Calendar, luxonLocalizer, Event as RBCEvent} from 'react-big-calendar';
-import React, {useCallback} from 'react';
 import { DateTime, Settings } from 'luxon';
+import { ProgramWithActivities } from '../types/api';
+import React from 'react';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
 import useApiData from '../helpers/useApiData';
-import { ProgramWithActivities } from '../types/api';
-import Program from './Program';
 
 Settings.defaultZone = 'America/Los_Angeles';
 const localizer = luxonLocalizer(DateTime);
 
-//   return (
-//     <Container fixed>
-//       <h2>Programs</h2>
-//       {programs.map(program => {
-//         return (
-//           <Program
-//             key={program.id}
-//             id={program.id}
-//             title={program.title}
-//             startDate={program.start_date}
-//             endDate={program.end_date}
-//             timeZone={program.time_zone}
-//             curriculumId={program.curriculum_id}
-//             activities={program.activities}
-//           />
-//         );
-//       })}
-//     </Container>
-//   );
-// };
-
 const CalendarPage = () => {
-
   const {
     data: programs,
     error,
@@ -51,30 +32,27 @@ const CalendarPage = () => {
   if (!programs) {
     return null;
   }
-
-  const programEventsActivities :  RBCEvent[] = programsList.data[0].activities.map((activity) => {
-    return (
-      {
+  const programEventsActivities: RBCEvent[] = programs[0].activities.map(
+    activity => {
+      return {
         title: activity.title,
         start: new Date(activity.start_time),
         end: new Date(activity.end_time),
-        description: activity.description_text
-      }
-    )
-  })
- 
+        description: activity.description_text,
+      };
+    }
+  );
   return (
     <Container fixed>
-        <Calendar 
+      <Calendar
         localizer={localizer}
         events={programEventsActivities}
         startAccessor="start"
         endAccessor="end"
         style={{ height: 500 }}
-        />
+      />
     </Container>
   );
 };
-
 
 export default CalendarPage;
