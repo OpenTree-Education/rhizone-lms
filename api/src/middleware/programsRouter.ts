6f8bc9b48ecf98722a55e6b99c3dfd6be8c7444f
Spencer W <4006420+seidior@users.noreply.github.com>
2022-11-22 17:00:13 -0800

    #429 Update programsRouter to use list vs find
    Update programsRouter and its test file to use the new listProgramsWithActivities instead of findProgramWithActivities.

--- api/src/middleware/programsRouter.ts
+++ api/src/middleware/programsRouter.ts
@@ -1,22 +1,11 @@
 import { Router } from 'express';
 
 import { collectionEnvelope } from './responseEnvelope';
-import {
-  findProgramWithActivities,
-  listProgramsWithActivities,
-} from '../services/programsService';
+import { listProgramsWithActivities } from '../services/programsService';
 
 const programsRouter = Router();
 
 programsRouter.get('/', async (req, res) => {
-  const firstProgramId = 1;
-  const programsWithActivities = [
-    await findProgramWithActivities(firstProgramId),
-  ];
-  res.json(collectionEnvelope(programsWithActivities, 1));
-});
-
-programsRouter.get('/test', async (req, res) => {
   const programsWithActivities = await listProgramsWithActivities();
   res.json(
     collectionEnvelope(programsWithActivities, programsWithActivities.length)
