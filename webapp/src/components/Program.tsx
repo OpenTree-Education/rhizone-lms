4d2c4c7ebc62bb215b5b006a08b8aba840d8da5c
Spencer W <4006420+seidior@users.noreply.github.com>
2022-11-23 20:52:04 -0800

    #428 Transform Program into a class
    The return value of Program() and the name didn’t match up before; `Program` didn’t return a Program, it returned a calendar.
    
    Switching Program to a class allows us to use it both as a data type and as a way to show the calendar, thereby supporting being able to switch programs and show a different calendar.
    
    (However, if we want to make this mutable... that’ll be more work down the road.)

--- webapp/src/components/Program.tsx
+++ webapp/src/components/Program.tsx
@@ -1,22 +1,10 @@
 import React from 'react';
-import FormControl from '@mui/material/FormControl';
-import InputLabel from '@mui/material/InputLabel';
-import MenuItem from '@mui/material/MenuItem';
-import Select from '@mui/material/Select';
-import Stack from '@mui/material/Stack';
+import { decodeHTML } from 'entities';
 import { DateTime } from 'luxon';
-import {
-  Calendar,
-  luxonLocalizer,
-  Event as RBCEvent,
-} from 'react-big-calendar';
+import { Calendar, luxonLocalizer } from 'react-big-calendar';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
-import { decodeHTML } from "entities";
 
-import { formatDate } from '../helpers/dateTime';
 import { EntityId, ProgramActivity } from '../types/api';
-
-const localizer = luxonLocalizer(DateTime);
 
 interface ProgramProps {
   id: EntityId;
@@ -26,52 +14,38 @@ interface ProgramProps {
   activities: ProgramActivity[];
 }
 
-const Program = ({
-  id,
-  title,
-  startDate,
-  endDate,
-  activities,
-}: ProgramProps) => {
-  const programId = id as number;
+export class Program {
+  id: EntityId;
+  title: string;
+  startDate: string;
+  endDate: string;
+  activities: ProgramActivity[];
 
-  const programEventsActivities: RBCEvent[] = activities.map(activity => {
-    return {
+  constructor({ id, title, startDate, endDate, activities }: ProgramProps) {
+    this.id = id;
+    this.title = title;
+    this.startDate = startDate;
+    this.endDate = endDate;
+    this.activities = activities;
+  }
+  activitiesForCalendar() {
+    return this.activities.map(activity => ({
       title: decodeHTML(activity.title),
       start: new Date(activity.start_time),
       end: new Date(activity.end_time),
       description: decodeHTML(activity.description_text),
       allDay: !activity.duration,
-    };
-  });
-  return (
-    <>
-      <Stack direction="row" justifyContent="flex-start" alignItems="center">
-        <FormControl sx={{ mb: 0.6, minWidth: 450, maxWidth: 600 }}>
-          <InputLabel id="demo-simple-select-label">Program</InputLabel>
-          <Select
-            labelId="program-select-label"
-            id="program-select"
-            value={programId}
-            label="Program"
-          >
-            <MenuItem value={programId}>
-              <strong>{title}</strong> ({formatDate(startDate)} &ndash;{' '}
-              {formatDate(endDate)})
-            </MenuItem>
-          </Select>
-        </FormControl>
-      </Stack>
-
+    }));
+  }
+  getCalendar() {
+    return (
       <Calendar
-        localizer={localizer}
-        events={programEventsActivities}
+        localizer={luxonLocalizer(DateTime)}
+        events={this.activitiesForCalendar()}
         startAccessor="start"
         endAccessor="end"
         style={{ height: 500 }}
       />
-    </>
-  );
-};
-
-export default Program;
+    );
+  }
+}
