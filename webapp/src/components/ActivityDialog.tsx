c50216a1bfea9968c414aeee4ee31715c4722c5c
Phoebe <phyuhala@gmail.com>
2022-11-24 16:40:44 -0800

    #433 Add styling for activity dialog

--- webapp/src/components/ActivityDialog.tsx
+++ webapp/src/components/ActivityDialog.tsx
@@ -5,7 +5,7 @@ import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
 import DialogContentText from '@mui/material/DialogContentText';
 import DialogTitle from '@mui/material/DialogTitle';
-import { ProgramActivity } from '../types/api';
+import Divider from '@mui/material/Divider';
 
 interface ActivityDialogProps {
   show: boolean;
@@ -30,22 +30,51 @@ const ActivityDialog = ({
       aria-labelledby="alert-dialog-title"
       aria-describedby="alert-dialog-description"
     >
-      <h1>Title: {contents.title}</h1>
+      <DialogTitle id="alert-dialog-title" sx={{ textAlign: 'center' }}>
+        {contents.title}
+      </DialogTitle>
 
-      {contents.start_time?.toLocaleDateString() ===
-      contents.end_time?.toLocaleDateString() ? (
-        <h4>Date: {contents.start_time?.toLocaleDateString()} </h4>
-      ) : (
-        <h4>
-          Date: {contents.start_time?.toLocaleDateString()} -{' '}
-          {contents.end_time?.toLocaleDateString()}
-        </h4>
-      )}
-      <h4>
-        Time: {contents.start_time?.toLocaleTimeString()} -{' '}
-        {contents.end_time?.toLocaleTimeString()}
-      </h4>
-      <p>Desciption:{contents.description_text}</p>
+      <Divider />
+
+      <DialogContent>
+        {contents.start_time?.toLocaleDateString() ===
+        contents.end_time?.toLocaleDateString() ? (
+          <DialogContentText id="alert-dialog-description">
+            <b>Date:</b> {contents.start_time?.toLocaleDateString()}
+          </DialogContentText>
+        ) : (
+          <DialogContentText id="alert-dialog-description">
+            <b>Date:</b> {contents.start_time?.toLocaleDateString()} -{' '}
+            {contents.end_time?.toLocaleDateString()}
+          </DialogContentText>
+        )}
+        <DialogContentText id="alert-dialog-description">
+          <b>Time:</b> {contents.start_time?.toLocaleTimeString()} -{' '}
+          {contents.end_time?.toLocaleTimeString()}
+        </DialogContentText>
+
+        <DialogContentText id="alert-dialog-description">
+          <b>Desciption:</b> {contents.description_text}
+        </DialogContentText>
+      </DialogContent>
+
+      <Divider />
+
+      <DialogActions>
+        <Button
+          onClick={handleClose}
+          sx={{
+            backgroundColor: 'primary.main',
+            color: 'common.white',
+            '&:hover': {
+              backgroundColor: 'primary.main',
+              color: 'common.white',
+            },
+          }}
+        >
+          Close
+        </Button>
+      </DialogActions>
     </Dialog>
   );
 };
