df25f916a9e1d3680d8b1d5971866aaeb2194b53
Ruthie Rabinovitch <rrabinovitch1@gmail.com>
2022-11-30 15:09:06 -0700

    #455 run linter

--- api/src/middleware/programsRouter.ts
+++ api/src/middleware/programsRouter.ts
@@ -1,7 +1,10 @@
 import { Router } from 'express';
 
 import { collectionEnvelope, itemEnvelope } from './responseEnvelope';
-import { listProgramsWithActivities, getParticipantActivityCompletion } from '../services/programsService'; // add participantActivity functions
+import {
+  listProgramsWithActivities,
+  getParticipantActivityCompletion,
+} from '../services/programsService'; // add participantActivity functions
 
 const programsRouter = Router();
 
@@ -12,27 +15,34 @@ programsRouter.get('/', async (req, res) => {
   );
 });
 
-programsRouter.get('/activityStatus/:programId/:activityId', async (req, res, next) => {
-  const { programId, activityId } = req.params;
-  const { principalId } = req.session;
+programsRouter.get(
+  '/activityStatus/:programId/:activityId',
+  async (req, res, next) => {
+    const { programId, activityId } = req.params;
+    const { principalId } = req.session;
 
-  let participantActivityId;
-  let activityCompletionStatus; // alternative variable names: isCompleted; completionStatus
+    let participantActivityId;
+    let activityCompletionStatus; // alternative variable names: isCompleted; completionStatus
 
- // checks:
-  // program exists
-  // activity exists
-  // curriculumId of program matches curriculumId of activity
-  // there's a ParticipantActivities record associated with the specified principalId, programId, and activityId
-  
-  try {
-    activityCompletionStatus = await getParticipantActivityCompletion(principalId, Number(programId), Number(activityId))
-  } catch (err) {
-    next(err);
-    return;
+    // checks:
+    // program exists
+    // activity exists
+    // curriculumId of program matches curriculumId of activity
+    // there's a ParticipantActivities record associated with the specified principalId, programId, and activityId
+
+    try {
+      activityCompletionStatus = await getParticipantActivityCompletion(
+        principalId,
+        Number(programId),
+        Number(activityId)
+      );
+    } catch (err) {
+      next(err);
+      return;
+    }
+
+    res.json(itemEnvelope({ status: activityCompletionStatus }));
   }
-
-  res.json(itemEnvelope({ status: activityCompletionStatus }));
-});
+);
 
 export default programsRouter;
