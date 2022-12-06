48aa8099dd0069733a6b1a5d94cfd5847fa856dc
Ruthie Rabinovitch <rrabinovitch1@gmail.com>
2022-12-06 15:27:58 -0700

    #455 clean up comments, add id number validation, note return values we're expecting from service functions

--- api/src/middleware/programsRouter.ts
+++ api/src/middleware/programsRouter.ts
@@ -1,7 +1,7 @@
 import { Router } from 'express';
 
 import { collectionEnvelope, itemEnvelope } from './responseEnvelope';
-import { NotFoundError, ValidationError } from './httpErrors';
+import { BadRequestError, NotFoundError, ValidationError } from './httpErrors';
 import {
   listProgramsWithActivities,
   getParticipantActivityCompletion,
@@ -23,10 +23,18 @@ programsRouter.get(
     const { programId, activityId } = req.params;
     const { principalId } = req.session;
 
-    // q: should these be moved into the try/catch statement so that an error is thrown if the id params cant be translated into a num?
-    // a: Good thinking! Check out line 12 in questionnairesRouter for an example of how to handle this without try/catch.
     const programIdNum = Number(programId);
     const activityIdNum = Number(activityId);
+
+    if(!Number.isInteger(programIdNum) || programIdNum < 1) {
+      next(new BadRequestError(`"${programIdNum}" is not a valid program id.`));
+      return;
+    }
+
+    if(!Number.isInteger(activityIdNum) || activityIdNum < 1) {
+      next(new BadRequestError(`"${activityId}" is not a valid activity id.`));
+      return;
+    }
 
     let activityCompletionStatus;
 
@@ -36,12 +44,14 @@ programsRouter.get(
         programIdNum,
         activityIdNum
       );
+      // return value should look like: { participantActivityId: x, completedStatus: boolean}
+      // if a participantActivityId does not exist: value is null or object/hash is empty?
     } catch (error) {
       next(error);
       return;
     }
-    // TODO: activityCompletionStatus should always be true/false, since we've decided not to do any error checking on the backend
-    if (!activityCompletionStatus) {
+
+    if (!activityCompletionStatus) { // value will be empty if service function doesn't successfully locate a participant activity
       next(
         new NotFoundError(
           `A participant activity with the activity id "${activityIdNum}" could not be found.`
@@ -67,18 +77,20 @@ programsRouter.put(
     // request body shoud look like: {"completed": true}
     const { principalId } = req.session;
 
-    // TODO: see comment from line 31 above
     const programIdNum = Number(programId);
     const activityIdNum = Number(activityId);
 
-    let updatedCompletionStatus;
-
-    // q: will this also address a case in which a completed value is not included in the request body?
-    // a: if the body doesn't contain a key called "completed", then the value of completed will be null
-    if (typeof completed !== 'boolean') {
-      next(new ValidationError('`completed` must be a boolean'));
+    if (!Number.isInteger(programIdNum) || programIdNum < 1) {
+      next(new BadRequestError(`"${programIdNum}" is not a valid program id.`));
       return;
     }
+
+    if (!Number.isInteger(activityIdNum) || activityIdNum < 1) {
+      next(new BadRequestError(`"${activityId}" is not a valid activity id.`));
+      return;
+    }
+
+    let updatedCompletionStatus;
 
     try {
       updatedCompletionStatus = await setParticipantActivityCompletion(
@@ -87,10 +99,6 @@ programsRouter.put(
         activityIdNum,
         completed
       );
-
-      // TODO: this function should return the row ID if successful. send back an error if it isn't.
-      if (typeof updatedCompletionStatus !== 'number') {
-      }
     } catch (error) {
       next(error);
       return;
