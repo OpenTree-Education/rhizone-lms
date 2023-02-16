0d8aad71cd6cfa771e17e1cf132f662b6068fb4a
Nataliia Sokolova <natalia.sokolova.ca@gmail.com>
2023-02-15 18:11:05 -0800

    did code refactoring in card and page components to pass props

--- webapp/src/components/AssessmentCard.tsx
+++ webapp/src/components/AssessmentCard.tsx
@@ -7,80 +7,96 @@ import { Button } from '@mui/material';
 import { blue } from '@mui/material/colors';
 import { useState } from 'react';
 import { Box } from '@mui/system';
+import { Margin } from '@mui/icons-material';
 
+interface AssessmentCardProps {
+  id: string;
+  title: string;
+  description: string;
+}
 
-export default function AssessmentCard() {
-    const assessments = [
-  {
-    "id": 1,
-    "title": "Introduction to HTML",
-    "description": "Learn the basics of HTML, the markup language used to create web pages.",
-    "score": 92,
-    "timestamp": "2023-02-15T13:30:00Z"
-  },
-  {
-    "id": 2,
-    "title": "CSS Layouts",
-    "description": "Explore different techniques for laying out web pages using CSS.",
-    "score": 85,
-    "timestamp": "2023-02-14T10:15:00Z"
-  },
-  {
-    "id": 3,
-    "title": "JavaScript Fundamentals",
-    "description": "Learn the basics of JavaScript, the programming language used to add interactivity to web pages.",
-    "score": 97,
-    "timestamp": "2023-02-12T16:45:00Z"
-  }
-]
-
+export default function AssessmentCard({
+  id,
+  title,
+  description,
+}: AssessmentCardProps) {
+  const assessments = [
+    {
+      id: 1,
+      title: 'Introduction to HTML',
+      description:
+        'Learn the basics of HTML, the markup language used to create web pages.',
+      score: 92,
+      timestamp: '2023-02-15T13:30:00Z',
+    },
+    {
+      id: 2,
+      title: 'CSS Layouts',
+      description:
+        'Explore different techniques for laying out web pages using CSS.',
+      score: 85,
+      timestamp: '2023-02-14T10:15:00Z',
+    },
+    {
+      id: 3,
+      title: 'JavaScript Fundamentals',
+      description:
+        'Learn the basics of JavaScript, the programming language used to add interactivity to web pages.',
+      score: 97,
+      timestamp: '2023-02-12T16:45:00Z',
+    },
+  ];
 
   return (
-    <Box>  
-        {assessments.length === 0 && <p>There are no upcomming assesments for you.</p>}
-        {assessments.map(({id, title, description, score,timestamp })=>(
-
-         <Paper key={id}
-      sx={{
-        p: 2,
-        margin: 'auto',
-        marginBottom: 4,
-        width: '100%',
-        flexGrow: 1,
-        border: blue,
-        backgroundColor: (theme) =>
-          theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
-      }}
-    >
+    <Box>
+      {/* {assessments.length === 0 && (
+        <p>There are no upcomming assesments for you.</p>
+      )}
+      {assessments.map(({ id, title, description, score, timestamp }) => ( */}
+      <Paper
+        key={id}
+        sx={{
+          p: 2,
+          margin: 'auto',
+          marginBottom: 4,
+          width: '100%',
+          borderRadius: '10px',
+          flexGrow: 1,
+          boxShadow: 'none',
+          border: '1.5px solid #2196f3',
+          backgroundColor: theme =>
+            theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+        }}
+      >
         <Grid container spacing={2}>
-            <Grid item xs={12} sm container>
-              <Grid item xs container direction="column" spacing={2}>
-                <Grid item xs>
-                  <Typography gutterBottom variant="subtitle1" component="div">
-                    {title}
-                  </Typography>
-                  <Typography variant="body2" color="text.secondary">
-                    Date/Time
-                  </Typography>
-                </Grid>
-                <Grid item>
-                  <Typography sx={{ cursor: 'pointer' }} variant="body2">
-                    {description}
-                  </Typography>
-                </Grid>
+          <Grid item xs={12} sm container>
+            <Grid item xs container direction="column" spacing={2}>
+              <Grid item xs>
+                <Typography gutterBottom variant="subtitle1" component="div">
+                  {title}
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                  Date/Time
+                </Typography>
               </Grid>
               <Grid item>
-                <Typography variant="subtitle1" component="div">
-                  {score} %
+                <Typography sx={{ cursor: 'pointer' }} variant="body2">
+                  {description}
                 </Typography>
               </Grid>
             </Grid>
-          </Grid><Button variant="contained">Start</Button> </Paper>))}
-   
+            <Grid item>
+              <Typography variant="subtitle1" component="div">
+                {/* {score} % */}
+              </Typography>
+            </Grid>
+          </Grid>
+        </Grid>
+        <Box m={1} display="flex" justifyContent="flex-end" alignItems="center">
+          <Button variant="contained">Start</Button>
+        </Box>
+      </Paper>
+      {/* ))} */}
     </Box>
-    
-   
   );
 }
-
-
--- webapp/src/components/AssessmentPage.tsx
+++ webapp/src/components/AssessmentPage.tsx
@@ -1,27 +1,77 @@
-
-import { Typography, Box } from "@mui/material";
-import React from "react";
-import AssessmentCard from "./AssessmentCard";
-
-
+import { Typography, Box } from '@mui/material';
+import React from 'react';
+import AssessmentCard from './AssessmentCard';
 
 const AssessmentPage = () => {
+  const assessments = [
+    {
+      id: 1,
+      title: 'Introduction to HTML',
+      description:
+        'Learn the basics of HTML, the markup language used to create web pages.',
+      score: 92,
+      timestamp: '2023-02-15T13:30:00Z',
+    },
+    {
+      id: 2,
+      title: 'CSS Layouts',
+      description:
+        'Explore different techniques for laying out web pages using CSS.',
+      score: 85,
+      timestamp: '2023-02-14T10:15:00Z',
+    },
+    {
+      id: 3,
+      title: 'JavaScript Fundamentals',
+      description:
+        'Learn the basics of JavaScript, the programming language used to add interactivity to web pages.',
+      score: 97,
+      timestamp: '2023-02-12T16:45:00Z',
+    },
+  ];
 
+  return (
+    <>
+      <Box ml={10} mr={10}>
+        {' '}
+        <Typography variant="h4" mt={5} mb={5}>
+          Hi User,
+        </Typography>
+        <Typography mb={5}>
+          Welcome to the assessment page! We are glad to have you here. This
+          page is designed to help you keep track of your past and upcoming
+          assessments, including the grades you received and any feedback from
+          assessors. We understand that assessments can be an important part of
+          your learning journey, and we want to make sure you have easy access
+          to all the information you need to stay on top of your progress.
+          Please feel free to explore the page and let us know if you have any
+          questions or feedback. We are here to help you succeed!
+        </Typography>
+        <Box>
+          <Typography variant="h5" color={'#2196f3'} mb={2} fontWeight={600}>
+            {' '}
+            Upcoming assessments
+          </Typography>
+          <AssessmentCard
+            id="1"
+            title="Introduction to HTML"
+            description="Learn the basics of HTML, the markup language used to create web pages."
+          />
+        </Box>
+        <Box>
+          <Typography variant="h5" color={'#2196f3'} mb={2} fontWeight={600}>
+            {' '}
+            Upcoming assessments
+          </Typography>
+          <AssessmentCard
+            id="2"
+            title="CSS Layouts"
+            description="Explore different techniques for laying out web pages using CSS."
+          />
+        </Box>
+      </Box>
+    </>
+  );
+};
 
-
-
-
-
-return (
-    <> 
-    <Box ml={10} mr={10}> <Typography variant="h4" mt={5} mb={5}>Hi User,</Typography>
-<Typography mb={5}>Welcome to the assessment page! We are glad to have you here. This page is designed to help you keep track of your past and upcoming assessments, including the grades you received and any feedback from assessors. We understand that assessments can be an important part of your learning journey, and we want to make sure you have easy access to all the information you need to stay on top of your progress. Please feel free to explore the page and let us know if you have any questions or feedback. We are here to help you succeed!
-</Typography>
-<AssessmentCard/>
-</Box>   
-</>
-
-);
-}
-
-export default AssessmentPage
+export default AssessmentPage;
