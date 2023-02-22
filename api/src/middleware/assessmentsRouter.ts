a69d6d42a4ca00718b12a357871ae13ccf5df379
Katy Harrod <m.catherine.harrod@gmail.com>
2023-02-22 16:18:50 -0700

    #506 api routes added

--- api/src/middleware/assessmentsRouter.ts
+++ api/src/middleware/assessmentsRouter.ts
@@ -6,4 +6,44 @@ assessmentsRouter.get('/', (req, res) => {
   const response = { behaviour: 'list of assessments' };
   res.status(200).json(itemEnvelope(response));
 });
+
+assessmentsRouter.post('/', (req, res) => {
+    const response = { behaviour: 'create new assessment' };
+    res.status(200).json(itemEnvelope(response));
+  });
+  
+assessmentsRouter.get('/:id', (req, res) => {
+    const response = { behaviour: 'view a single assessment' };
+    res.status(200).json(itemEnvelope(response));
+});
+
+assessmentsRouter.put('/:id', (req, res) => {
+    const response = { behaviour: 'Edits an assessment in the system' };
+    res.status(200).json(itemEnvelope(response));
+});
+
+assessmentsRouter.delete('/:id', (req, res) => {
+    const response = { behaviour: '“Deletes” an assessment in the system' };
+    res.status(200).json(itemEnvelope(response));
+});
+
+assessmentsRouter.get('/:id/submissions/:id', (req, res) => {
+    const response = { behaviour: 'Returns the submission information (metadata, answers, etc)' };
+    res.status(200).json(itemEnvelope(response));
+});
+
+assessmentsRouter.put('/:id/submissions/:id', (req, res) => {
+    const response = { behaviour: 'submits their answer for assessment' };
+    res.status(200).json(itemEnvelope(response));
+});
+
+assessmentsRouter.get('/:id/submissions/new', (req, res) => {
+    const response = { behaviour: 'creates a new draft submission' };
+    res.status(200).json(itemEnvelope(response));
+});
+
+assessmentsRouter.get('programs/:id/certificate/:principal_id', (req, res) => {
+    const response = { behaviour: 'brings up certificate' };
+    res.status(200).json(itemEnvelope(response));
+});
 export default assessmentsRouter;
