5ff97a28cf525e74cb063dd53b16039b58498d8c
Zeyuan Li <li.ze@husky.neu.edu>
2023-02-22 15:32:16 -0600

    #507 Add assessment tab and dummy table

--- webapp/src/components/AssessmentPage.tsx
+++ webapp/src/components/AssessmentPage.tsx
@@ -1,10 +1,197 @@
-import { Box, Container, Grid } from '@mui/material';
+import { Box, Container, Grid, Stack } from '@mui/material';
 import React, { useState } from 'react';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import Link from '@mui/material/Link';
+
+import AddCircleOutlineOutlinedIcon from '@mui/icons-material/AddCircleOutlineOutlined';
+import AlarmOnOutlinedIcon from '@mui/icons-material/AlarmOnOutlined';
+import Badge, { BadgeProps } from '@mui/material/Badge';
+import { styled } from '@mui/material/styles';
+import IconButton from '@mui/material/IconButton';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import DoneAllOutlinedIcon from '@mui/icons-material/DoneAllOutlined';
+import DoneOutlinedIcon from '@mui/icons-material/DoneOutlined';
+import ScheduleOutlinedIcon from '@mui/icons-material/ScheduleOutlined';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import CancelOutlinedIcon from '@mui/icons-material/CancelOutlined';
+import ArchiveOutlinedIcon from '@mui/icons-material/ArchiveOutlined';
+import UpcomingOutlinedIcon from '@mui/icons-material/UpcomingOutlined';
+import LockClockOutlinedIcon from '@mui/icons-material/LockClockOutlined';
+
+import Typography from '@mui/material/Typography';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import ArrowForwardIosSharpIcon from '@mui/icons-material/ArrowForwardIosSharp';
+import Accordion from '@mui/material/Accordion';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+
+function createData(
+  name: string,
+  date:string,
+  grade:string
+) {
+  return { date, name, grade };
+}
+
+const rows = [
+  createData("Assessment 1","25-2-2023","yes"),
+  createData("Assessment 2","26-2-2023","no"),
+  
+
+];
+
+function a11yProps(index: number) {
+  return {
+    id: `vertical-tab-${index}`,
+    'aria-controls': `vertical-tabpanel-${index}`,
+  };
+}
+
+const StyledBadge = styled(Badge)<BadgeProps>(({ theme }) => ({
+  '& .MuiBadge-badge': {
+    border: `2px solid ${theme.palette.background.paper}`,
+    padding: '0 4px',
+  },
+}));
 
 const AssessmentPage = () => {
+
+  const [expanded, setExpanded] = React.useState<string | false>('panel1');
+
+  const handleChange =
+  (panel: string) => (event: React.SyntheticEvent, newExpanded: boolean) => {
+    setExpanded(newExpanded ? panel : false);
+  };
+  const [value, setValue] = React.useState(0);
+
+  const handleChangeTab = (event: React.SyntheticEvent, newValue: number) => {
+    setValue(newValue);
+  };
+
   return (
-    <Container fixed>
-      <h1>Assessment</h1>
+    <Container>
+      <Stack
+        direction={{ xs: 'column', md: 'row' }}
+        justifyContent="space-between"
+        alignItems={{ xs: 'flex-start', md: 'center' }}
+      >
+        <h1>Assessment</h1> 
+      </Stack>
+
+          {/* <Accordion>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel1a-content"
+          id="panel1a-header"
+        >
+          <Typography>Active</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+        <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 600 }} size="small" aria-label="a dense table">
+        <TableHead>
+          <TableRow>
+            <TableCell align="right"  >date</TableCell>
+            <TableCell align="right">name</TableCell>
+            <TableCell align="right">Grade/Ungrade</TableCell>
+            
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => (
+            <TableRow
+              key={row.name}  
+              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+            >
+             
+              <TableCell align="right">{row.date}</TableCell>
+              <TableCell align="right">
+               
+                {row.name}
+                </TableCell>
+              <TableCell align="right">{row.grade}</TableCell>
+              
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel2a-content"
+          id="panel2a-header"
+        >
+          <Typography>Past</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            malesuada lacus ex, sit amet blandit leo lobortis eget.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel3a-content"
+          id="panel3a-header"
+        >
+          <Typography>Upcoming</Typography>
+        </AccordionSummary>
+      </Accordion> */}
+
+      <Box sx={{ width: '100%', bgcolor: 'background.paper'}}>
+        <Tabs value={value} onChange={handleChangeTab} >
+          <Tab icon={<StyledBadge badgeContent={4} color="primary"><ScheduleOutlinedIcon /></StyledBadge>} iconPosition="start" label="Active" />
+          <Tab icon={<ArchiveOutlinedIcon/>} iconPosition="start" label="Past"  />
+          <Tab icon={<UpcomingOutlinedIcon/>} iconPosition="start" label="Upcoming"  />
+        </Tabs>
+      </Box>
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 600 }}  aria-label="a dense table">
+          <TableHead>
+            <TableRow>
+              <TableCell>Status</TableCell>
+              <TableCell>Title</TableCell>
+              <TableCell>Type</TableCell>
+              <TableCell>Due Date</TableCell>
+              <TableCell>Test Duration</TableCell>
+              <TableCell>Submit Date</TableCell>
+              <TableCell>Score</TableCell>
+              <TableCell>Available Date</TableCell>
+              <TableCell>Action</TableCell>              
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {rows.map((row) => (
+              <TableRow
+                key={row.name}  
+                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+              >
+              
+                <TableCell >{row.date}</TableCell>
+                <TableCell >
+                
+                  {row.name}
+                  </TableCell>
+                <TableCell >{row.grade}</TableCell>
+                
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
     </Container>
   );
 };
