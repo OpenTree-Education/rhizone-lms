006b4d0d2e58f421a517655d8e070b2aa0c3ff54
Spencer W <4006420+seidior@users.noreply.github.com>
2023-02-24 12:31:51 -0800

    #505 Revert changes to v010 file

--- db/migration/V010__insert_default_questionnaire.sql
+++ db/migration/V010__insert_default_questionnaire.sql
@@ -1,10 +1,10 @@
-INSERT INTO questionnaires ()
-VALUES ();
+INSERT INTO questionnaires () VALUES
+  ();
 SET @questionnaire_id = LAST_INSERT_ID();
-INSERT INTO settings (property, content, category) VALUES 
-('default_questionnaire_id',CAST(@questionnaire_id AS CHAR),'webapp');
-INSERT INTO prompts (label, query_text, sort_order, questionnaire_id)
-VALUES ('Outlook', 'How are you feeling about your current endeavours?', 1, @questionnaire_id);
+INSERT INTO settings (property, content, category) VALUES
+  ('default_questionnaire_id', CAST(@questionnaire_id AS CHAR), 'webapp');
+INSERT INTO prompts (label, query_text, sort_order, questionnaire_id) VALUES
+  ('Outlook', 'How are you feeling about your current endeavours?', 1, @questionnaire_id);
 SET @prompt_id = LAST_INSERT_ID();
 INSERT INTO options (label, numeric_value, sort_order, prompt_id) VALUES
   ('Very discouraged', -2, 1, @prompt_id),
