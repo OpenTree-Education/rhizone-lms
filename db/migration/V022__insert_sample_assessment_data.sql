42cb898f4a625bbdc23cde82074679cb221cde61
Nataliia Sokolova <natalia.sokolova.ca@gmail.com>
2023-02-23 18:45:41 -0800

    #505 formated V022 file

--- db/migration/V022__insert_sample_assessment_data.sql
+++ db/migration/V022__insert_sample_assessment_data.sql
@@ -3,53 +3,11 @@ VALUES (`quiz`),
     (`test`);
 INSERT INTO `curriculums` (`title`, `principal_id`)
 VALUES ("Beginner Web Development", 2);
-INSERT INTO `programs` (`title`,`start_date`,
-        `end_date`,
-        `time_zone`,
-        `principal_id`,
-        `curriculum_id`
-    )
-VALUES (
-        "Spring 2023",
-        "2023-02-06",
-        "2023-03-31",
-        "America/Vancouver",
-        2,
-        3
-    );
-INSERT INTO `activities` (
-        `title`,
-        `description_text`,
-        `curriculum_week`,
-        `curriculum_day`,
-        `start_time`,
-        `end_time`,
-        `duration`,
-        `activity_type_id`,
-        `curriculum_id`
-    )
-VALUES (
-        "Assignment 1: React",
-        "Your assignment for week 1 learning.",
-        1,
-        5,
-        NULL,
-        NULL,
-        NULL,
-        1,
-        3
-    ),
-    (
-        "Quiz 1: SQL",
-        "A check on your SQL learning.",
-        2,
-        5,
-        NULL,
-        NULL,
-        NULL,
-        10,
-        3
-    ),
+INSERT INTO `programs` (`title`,`start_date`,`end_date`,`time_zone`, `principal_id`,`curriculum_id`)
+VALUES ("Spring 2023", "2023-02-06","2023-03-31", "America/Vancouver", 2, 3);
+INSERT INTO `activities` (`title`,`description_text`,`curriculum_week`,`curriculum_day`,`start_time`,`end_time`,`duration`,`activity_type_id`,`curriculum_id`)
+VALUES ("Assignment 1: React", "Your assignment for week 1 learning.", 1, 5, NULL, NULL, NULL, 1, 3),
+    ("Quiz 1: SQL", "A check on your SQL learning.", 2, 5, NULL, NULL, NULL, 10, 3),
     (
         "Test 1: SQL",
         "A check on your SQL learning -- for credit!",
@@ -64,7 +22,6 @@ VALUES (
 INSERT INTO `question_types` (`title`)
 VALUES ("multiple choice"),
     ("free response");
--- TODO: Insert the quiz and test. --
 INSERT INTO `curriculum_assessments` (
         `title`,
         `description`,
@@ -104,7 +61,6 @@ VALUES (
         2
     ),
 ;
--- DONE: Figure out what to insert here. --
 INSERT INTO `program_assessments` (
         `program_id`,
         `assessment_id`,
@@ -121,7 +77,6 @@ VALUES (
         '2023-02-22',
         '2023-02-23'
     );
--- TODO: Insert the other questions. --
 INSERT INTO `assessment_questions` (
         `assessment_id`,
         `title`,
@@ -132,7 +87,6 @@ INSERT INTO `assessment_questions` (
         `sort_order`
     )
 VALUES (1, "What is React?", NULL, 1, 4, 1, 1),
-    --question 2
     (
         1,
         "What is the purpose of JSX in React?",
@@ -142,7 +96,6 @@ VALUES (1, "What is React?", NULL, 1, 4, 1, 1),
         1,
         2
     ),
-    --question 3
     (
         1,
         "What is the virtual DOM in React?",
@@ -152,7 +105,6 @@ VALUES (1, "What is React?", NULL, 1, 4, 1, 1),
         1,
         3
     ),
-    --question 4
     (
         1,
         "What are React components",
@@ -162,7 +114,6 @@ VALUES (1, "What is React?", NULL, 1, 4, 1, 1),
         1,
         4
     ),
-    --question 5
     (
         1,
         "True or false? When lifting state up, you need to move the useState from a child component to a parent component.",
@@ -172,7 +123,6 @@ VALUES (1, "What is React?", NULL, 1, 4, 1, 1),
         1,
         5
     ),
-    --question 6
     (
         1,
         "Which of the following options describe what a prop is in React",
@@ -182,7 +132,6 @@ VALUES (1, "What is React?", NULL, 1, 4, 1, 1),
         1,
         6
     ),
-    --question 7
     (
         1,
         "If the state variable holds an array or a string value, once you pass that state via props from a parent to a child, you can then read the length property from the received prop in the child component",
@@ -192,17 +141,15 @@ VALUES (1, "What is React?", NULL, 1, 4, 1, 1),
         1,
         7
     ),
-    --question 8
     (
         1,
-        "Write a React component that displays the text " Hello,
-        World ! " on the screen." NULL,
+        "Write a React component that displays the text "Hello,
+        World!" on the screen." NULL,
         2,
         NULL,
         1,
         8
     ),
-    --question 9
     (
         1,
         "How does React differ from other JavaScript frameworks?" NULL,
@@ -211,7 +158,6 @@ VALUES (1, "What is React?", NULL, 1, 4, 1, 1),
         1,
         9
     ),
-    --question 10
     (
         1,
         "How does React differ from other JavaScript frameworks?" NULL,
@@ -220,11 +166,7 @@ VALUES (1, "What is React?", NULL, 1, 4, 1, 1),
         1,
         10
     ),
-    
     --QUIZ 
-
-    
-    --quiz 1
     (
         2,
         "A JOIN clause is used to combine rows from two or more tables, based on a related column between them. Which command is not a Join type in SQL?",
@@ -234,9 +176,6 @@ VALUES (1, "What is React?", NULL, 1, 4, 1, 1),
         1,
         1
     ),
-
-    --quiz 2
-
     (
         2,
         "Which statement is wrong?" , 
@@ -246,9 +185,6 @@ VALUES (1, "What is React?", NULL, 1, 4, 1, 1),
         1,
         2
     ),
-
-    --quiz 3
-
         (
         2,
         "How can we add a column to a table?" , 
@@ -258,9 +194,6 @@ VALUES (1, "What is React?", NULL, 1, 4, 1, 1),
         1,
         3
     ),
-
-    --quiz 4
-
         (
         2,
         "Each table can contain more than one primary key." ,
@@ -270,59 +203,37 @@ VALUES (1, "What is React?", NULL, 1, 4, 1, 1),
         1,
         4
     ),
-
-
     --TEST 
-    (
-        3,
-        "What is MySQL?" NULL,
-        1,
-        4,
-        1,
-        1
-    ),
-    --question 2
-    (
-        3,
-        "True or false? INT and VARCHAR are some common data types in MySQL.",
-        NULL,
-        1,
-        5,
-        1,
-        2
-    ),
-    --question 3
+    (3, "What is MySQL?" NULL, 1, 39, 1, 1),
+    (3, "True or false? INT and VARCHAR are some common data types in MySQL.", NULL, 1, 43, 1, 2),
     (
         3,
         'Which command is used to create a new database in MySQL?',
         NULL,
         1,
-        9,
+        47,
         1,
         3
     ),
-    --question 4
     (
         3,
         "COUNT, SUM, INSERT INTO  are some common MySQL aggregate functions.
 ",
         NULL,
         1,
-        14,
+        50,
         1,
         4
     ),
-    --question 5
     (
         3,
         "Which command is used to insert new data into a MySQL table?",
         NULL,
         1,
-        18,
+        54,
         1,
         5
     ),
-    --question 6
     (
         3,
         "Which command is used to delete a table from a MySQL database?",
@@ -332,59 +243,48 @@ VALUES (1, "What is React?", NULL, 1, 4, 1, 1),
         1,
         6
     ),
-    --question 7
     (
         3,
         "Which builtin MySQL function can be used to add every value from a column together in a query?" NULL,
         1,
-        23,
+        62,
         1,
         7
     ),
-    --question 8
     (
         3,
         "Which command is used to retrieve data from a MySQL table?" NULL,
         2,
-        NULL,
+        66,
         1,
         8
     ),
-    --question 9
     (
         3,
         "Which function is used to count the number of rows in a MySQL table?" NULL,
         2,
-        NULL,
+        67,
         1,
         9
     ),
-    --question 10
     (
         3,
         "Which keyword is used to specify the condition for a MySQL query?" NULL,
         2,
-        NULL,
+        68,
         1,
         10
     ),
-    -- TODO: Insert the other answers. --
 INSERT INTO `assessment_answers` (
         `question_id`,
         `title`,
         `description`,
         `sort_order`
     )
-VALUES (
-        1,
-        "A relational database management system",
-        NULL,
-        1
-    ),
+VALUES (1, "A relational database management system", NULL, 1),
     (1, "A database management system", NULL, 2),
     (1, "A web server software", NULL, 3),
     (1, "A front-end JavaScript library", NULL, 4),
-    --question 2
     (
         2,
         "To provide a syntax for writing HTML in JavaScript",
@@ -409,7 +309,6 @@ VALUES (
         NULL,
         4
     ),
-    --question 3
     (
         3,
         "A virtual representation of the actual HTML DOM",
@@ -434,7 +333,6 @@ VALUES (
         NULL,
         4
     ),
-    --question 4 (row 13-16)
     (
         4,
         "An object-oriented programming language feature",
@@ -459,10 +357,8 @@ VALUES (
         NULL,
         4
     ),
-    --question 5 (row 17-18)
     (5, "True", NULL, 1),
     (5, "False", NULL, 2),
-    --question 6 (row 19-22)
     (
         6,
         " A built-in method for creating React components",
@@ -487,10 +383,8 @@ VALUES (
         NULL,
         4
     ),
-    --question 7 (row 23-24)
     (7, "True", NULL, 1),
     (7, "False", NULL, 2),
-    --question 8 (row 25)
     (
         8,
         "const HelloWorld = () => {
@@ -499,14 +393,8 @@ VALUES (
         NULL,
         1
     ),
-    --question 8 (row 26)
     (9, "React differs from other JavaScript frameworks because it uses a component-based architecture, a virtual DOM, JSX syntax, unidirectional data flow, and is primarily focused on building UIs rather than providing a complete application framework. These features make it faster, more efficient, and more flexible than other frameworks.", NULL, 1),
     (10, "Benefits of using React include its modular and reusable components, efficient updates with virtual DOM, JSX syntax, and active community.", NULL, 1),
-
-
---quiz
-
---quiz 1 (row 28-31)
     (
         11,
         "Self Join",
@@ -531,9 +419,7 @@ VALUES (
         NULL,
         4
     ),
-
-
---quiz 2  (row 32-35)
+-- (row 32-35)
     (
         12,
         "TypeScript is known as Dynamically-typed language",
@@ -558,8 +444,7 @@ VALUES (
         NULL,
         4
     ),
-
---quiz 3  (row 36-39)
+-- (row 36-39)
     (
         13,
         "CREATE TABLE",
@@ -585,7 +470,7 @@ VALUES (
         4
     ),
 
---quiz 4  (row 37-38)
+--  (row 37-38)
     (
         14,
         "True",
@@ -598,16 +483,13 @@ VALUES (
         NULL,
         2
     ),
-
-
-
 -- TEST 
-
-(15, "A back-end programming language", NULL, 1),
+--(row 39-42)
+(15, "A relational database management system", NULL, 1),
     (15, "A programming language", NULL, 2),
     (15, "An operating system", NULL, 3),
     (15, "A web server", NULL, 4),
-    --question 16
+--(row 43-44)
     (
         16,
         "True",
@@ -620,7 +502,7 @@ VALUES (
         NULL,
         2
     ),
-    --question 17
+ --(row 45-48)
     (
         17,
         "CREATE TABLE",
@@ -629,8 +511,7 @@ VALUES (
     ),
     (
         17,
-        "CREATE INDEX
-",
+        "CREATE INDEX",
         NULL,
         2
     ),
@@ -646,7 +527,7 @@ VALUES (
         NULL,
         4
     ),
-    --question 18 (row)
+    --question 18 (row 49-50)
     (
         18,
         "True",
@@ -659,47 +540,17 @@ VALUES (
         NULL,
         2
     ),
-    --question 19 (row)
+    --question 19 (row 51-54)
     (19, "ADD DATA", NULL, 1),
     (19, "INSERT DATA", NULL, 2),
-    (
-        19,
-        "INSERT ROW",
-        NULL,
-        3
-    ),
-    (
-        19,
-        "INSERT INTO",
-        NULL,
-        4
-    ),
-    --question 20 (row)
-    (
-        20,
-        "DELETE TABLE",
-        NULL,
-        1
-    ),
-    (
-        20,
-        "DROP TABLE",
-        NULL,
-        2
-    ),
-    (
-        20,
-        "REMOVE TABLE",
-        NULL,
-        3
-    ),
-    (
-        20,
-        "ERASE TABLE",
-        NULL,
-        4
-    ),
-    --question 21 (row)
+    (19, "INSERT ROW", NULL, 3),
+    (19,"INSERT INTO", NULL, 4),
+    --question 20 (row 55-58)
+    (20, "DELETE TABLE", NULL, 1),
+    (20,"DROP TABLE", NULL, 2),
+    (20, "REMOVE TABLE", NULL, 3),
+    (20,"ERASE TABLE", NULL,4),
+    --question 21 (row 59-65)
     (21, "MAX", NULL, 1),
     (21, "TOGETHER", NULL, 2),
     (21, "TOTAL", NULL, 3),
