e1ce1827db6f0fb5db11f5a266facffedf50ce76
Katy Harrod <m.catherine.harrod@gmail.com>
2023-02-24 11:42:20 -0700

    #506 run yarn delint to fix spacing issues

--- api/src/middleware/assessmentsRouter.ts
+++ api/src/middleware/assessmentsRouter.ts
@@ -8,38 +8,40 @@ assessmentsRouter.get('/', (req, res) => {
 });
 
 assessmentsRouter.post('/', (req, res) => {
-    const response = { behaviour: 'create new assessment' };
-    res.status(200).json(itemEnvelope(response));
-  });
+  const response = { behaviour: 'create new assessment' };
+  res.status(200).json(itemEnvelope(response));
+});
 
 assessmentsRouter.get('/:id', (req, res) => {
-    const response = { behaviour: 'view a single assessment' };
-    res.status(200).json(itemEnvelope(response));
+  const response = { behaviour: 'view a single assessment' };
+  res.status(200).json(itemEnvelope(response));
 });
 
 assessmentsRouter.put('/:id', (req, res) => {
-    const response = { behaviour: 'Edits an assessment in the system' };
-    res.status(200).json(itemEnvelope(response));
+  const response = { behaviour: 'Edits an assessment in the system' };
+  res.status(200).json(itemEnvelope(response));
 });
 
 assessmentsRouter.delete('/:id', (req, res) => {
-    const response = { behaviour: '“Deletes” an assessment in the system' };
-    res.status(200).json(itemEnvelope(response));
+  const response = { behaviour: '“Deletes” an assessment in the system' };
+  res.status(200).json(itemEnvelope(response));
 });
 
 assessmentsRouter.get('/:id/submissions/:id', (req, res) => {
-    const response = { behaviour: 'Returns the submission information (metadata, answers, etc)' };
-    res.status(200).json(itemEnvelope(response));
+  const response = {
+    behaviour: 'Returns the submission information (metadata, answers, etc)',
+  };
+  res.status(200).json(itemEnvelope(response));
 });
 
 assessmentsRouter.put('/:id/submissions/:id', (req, res) => {
-    const response = { behaviour: 'submits their answer for assessment' };
-    res.status(200).json(itemEnvelope(response));
+  const response = { behaviour: 'submits their answer for assessment' };
+  res.status(200).json(itemEnvelope(response));
 });
 
 assessmentsRouter.get('/:id/submissions/new', (req, res) => {
-    const response = { behaviour: 'creates a new draft submission' };
-    res.status(200).json(itemEnvelope(response));
+  const response = { behaviour: 'creates a new draft submission' };
+  res.status(200).json(itemEnvelope(response));
 });
 
 export default assessmentsRouter;
