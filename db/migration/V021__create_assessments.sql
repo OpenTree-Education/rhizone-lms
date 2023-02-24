0d3d7ef1cfb221645f377fe19dfb1ee97ed4dc65
Paryssatis Khazaie <paryssatis@outlook.com>
2023-02-24 10:48:35 -0800

    #504 Address proposed changes

--- db/migration/V021__create_assessments.sql
+++ db/migration/V021__create_assessments.sql
@@ -1,5 +1,5 @@
 
-CREATE TABLE question_types (
+CREATE TABLE assessment_question_types (
   id BIGINT NOT NULL AUTO_INCREMENT,
   title TEXT NOT NULL,
   PRIMARY KEY (id)
@@ -17,22 +17,39 @@ CREATE TABLE program_participant_roles (
   PRIMARY KEY (id)
 );
 
+CREATE TABLE program_participants (
+  id BIGINT NOT NULL AUTO_INCREMENT,
+  principal_id BIGINT NOT NULL,
+  program_id BIGINT NOT NULL,
+  role_id BIGINT,
+  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  INDEX program_participants_principal_id (principal_id),
+  FOREIGN KEY (principal_id) REFERENCES principals(id),
+  INDEX program_participants_program_id (program_id),
+  FOREIGN KEY (program_id) REFERENCES programs(id) ON DELETE CASCADE,
+  INDEX program_participants_role_id (role_id),
+  FOREIGN KEY (role_id) REFERENCES roles(id) ON DELETE SET NULL,
+  PRIMARY KEY (id),
+  UNIQUE KEY `principals_and_programs` (`principal_id`,`program_id`)
+);
+
 CREATE TABLE curriculum_assessments (
   id BIGINT NOT NULL AUTO_INCREMENT,
   title TEXT NOT NULL,
-  description TEXT,
-  max_score INT NOT NULL,
-  max_num_submissions INT NOT NULL,
+  description TEXT NOT NULL,
+  max_score INT,
+  max_num_submissions INT NOT NULL DEFAULT 1,
   time_limit INT,
   curriculum_id BIGINT NOT NULL,
-  activity_id BIGINT NOT NULL,
+  activity_id BIGINT NULL,
   principal_id BIGINT NOT NULL,
   created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   INDEX curriculum_assessments_curriculum_id (curriculum_id),
-  FOREIGN KEY (curriculum_id) REFERENCES curriculums(id),
+  FOREIGN KEY (curriculum_id) REFERENCES curriculums(id) ON DELETE CASCADE,
   INDEX curriculum_assessments_activity_id (activity_id),
-  FOREIGN KEY (activity_id) REFERENCES activities(id),
+  FOREIGN KEY (activity_id) REFERENCES activities(id) ON DELETE SET NULL,
   INDEX curriculum_assessments_principal_id (principal_id),
   FOREIGN KEY (principal_id) REFERENCES principals(id),
   PRIMARY KEY (id)
@@ -47,41 +64,11 @@ CREATE TABLE program_assessments (
   created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   INDEX program_assessments_program_id (program_id),
-  FOREIGN KEY (program_id) REFERENCES programs(id),
+  FOREIGN KEY (program_id) REFERENCES programs(id) ON DELETE CASCADE,
   INDEX program_assessments_assessment_id (assessment_id),
-  FOREIGN KEY (assessment_id) REFERENCES curriculum_assessments(id),
-  PRIMARY KEY (id)
-);
-
-CREATE TABLE program_participants (
-  id BIGINT NOT NULL AUTO_INCREMENT,
-  principal_id BIGINT NOT NULL,
-  program_id BIGINT NOT NULL,
-  role_id BIGINT NOT NULL,
-  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  INDEX program_participants_principal_id (principal_id),
-  FOREIGN KEY (principal_id) REFERENCES principals(id),
-  INDEX program_participants_program_id (program_id),
-  FOREIGN KEY (program_id) REFERENCES programs(id),
-  PRIMARY KEY (id)
-);
-
-CREATE TABLE assessment_submissions (
-  id BIGINT NOT NULL AUTO_INCREMENT,
-  assessment_id BIGINT NOT NULL,
-  principal_id BIGINT NOT NULL,
-  state_id BIGINT NOT NULL,
-  score INT,
-  opened_at TIMESTAMP NOT NULL,
-  submitted_at TIMESTAMP NOT NULL,
-  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  INDEX assessment_submissions_assessment_id (assessment_id),
-  FOREIGN KEY (assessment_id) REFERENCES program_assessments(id),
-  INDEX assessment_submissions_principal_id (principal_id),
-  FOREIGN KEY (principal_id) REFERENCES principals(id),
-  PRIMARY KEY (id)
+  FOREIGN KEY (assessment_id) REFERENCES curriculum_assessments(id) ON DELETE CASCADE,
+  PRIMARY KEY (id),
+  UNIQUE KEY `program_and_assessment` (`program_id`,`assessment_id`)
 );
 
 CREATE TABLE assessment_questions (
@@ -89,29 +76,52 @@ CREATE TABLE assessment_questions (
   assessment_id BIGINT NOT NULL,
   title TEXT NOT NULL,
   description TEXT,
-  question_type_id BIGINT NOT NULL,
+  assessment_question_type_id BIGINT NOT NULL,
   correct_answer_id BIGINT,
   max_score INT,
   sort_order INT NOT NULL,
   created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP ON DELETE CASCADE,
   INDEX assessment_questions_assessment_id (assessment_id),
-  FOREIGN KEY (assessment_id) REFERENCES curriculum_assessments(id),
-  INDEX assessment_questions_question_type_id (question_type_id),
-  FOREIGN KEY (question_type_id) REFERENCES question_types(id),
-  PRIMARY KEY (id)
+  FOREIGN KEY (assessment_id) REFERENCES curriculum_assessments(id) ON DELETE CASCADE,
+  INDEX assessment_questions_assessment_question_type_id (assessment_question_type_id),
+  FOREIGN KEY (assessment_question_type_id) REFERENCES assessment_question_types(id),
+  INDEX assessment_questions_correct_answer_id (correct_answer_id),
+  FOREIGN KEY (correct_answer_id) REFERENCES correct_answers(id) ON DELETE SET NULL,
+  PRIMARY KEY (id),
+  UNIQUE KEY `assessment_and_question_number` (`assessment_id`,`sort_order`)
 );
 
 CREATE TABLE assessment_answers (
   id BIGINT NOT NULL AUTO_INCREMENT,
-  question_id BIGINT NOT NULL,
+  question_id BIGINT,
   title TEXT NOT NULL,
-  description TEXT NOT NULL,
+  description TEXT,
   sort_order INT NOT NULL,
   created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   INDEX assessment_answers_question_id (question_id),
-  FOREIGN KEY (question_id) REFERENCES assessment_questions(id),
+  FOREIGN KEY (question_id) REFERENCES assessment_questions(id) ON DELETE CASCADE,
+  PRIMARY KEY (id),
+  UNIQUE KEY `question_and_answer_order` (`question_id`,`sort_order`)
+);
+
+CREATE TABLE assessment_submissions (
+  id BIGINT NOT NULL AUTO_INCREMENT,
+  assessment_id BIGINT NOT NULL,
+  principal_id BIGINT NOT NULL,
+  assessment_submission_state_id BIGINT NOT NULL,
+  score INT,
+  opened_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  submitted_at TIMESTAMP,
+  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  INDEX assessment_submissions_assessment_id (assessment_id),
+  FOREIGN KEY (assessment_id) REFERENCES program_assessments(id),
+  INDEX assessment_submissions_principal_id (principal_id),
+  FOREIGN KEY (principal_id) REFERENCES principals(id),
+  INDEX assessment_submissions_assessment_submission_state_id (assessment_submission_state_id),
+  FOREIGN KEY (assessment_submission_state_id) REFERENCES assessment_submission_states(id),
   PRIMARY KEY (id)
 );
 
@@ -133,6 +143,16 @@ CREATE TABLE assessment_responses (
   INDEX assessment_responses_question_id (question_id),
   FOREIGN KEY (question_id) REFERENCES assessment_questions(id),
   INDEX assessment_responses_answer_id (answer_id),
-  FOREIGN KEY (answer_id) REFERENCES assessment_answers(id),
-  PRIMARY KEY (id)
+  FOREIGN KEY (answer_id) REFERENCES assessment_answers(id) ON DELETE SET NULL,
+  PRIMARY KEY (id),
+  UNIQUE KEY `submission_and_question` (`submission_id`,`question_id`)
 );
+
+ALTER TABLE questionnaires
+RENAME TO surveys;
+
+ALTER TABLE prompts
+RENAME TO survey_questions;
+
+ALTER TABLE options
+RENAME TO survey_answers;
