3b8e83dae061add5275db46a9fd73b05c5370e6e
Paryssatis Khazaie <paryssatis@outlook.com>
2023-02-28 13:40:01 -0800

    #515 Added sudocode

--- api/src/services/assessmentService.ts
+++ api/src/services/assessmentService.ts
@@ -4,7 +4,7 @@ import {ListAssessments} from '../models';
 // import { DateTime, Duration } from 'luxon';
 
 /**
- * Returns list of assessments in the database.
+ * (GET /assessments) Returns list of assessments in the database.
  *
  * @returns {ListAssessments[]} - list of assessments in the db
  * @param {number} principalId - the unique id for the user
@@ -31,3 +31,61 @@ export const listAssessmentsByParticipant = async (principalId: number) => {
     .whereIn('program_id', programIds); //array of program ids);
   return assessmentsList;
 };
+
+/**
+ * (POST /assessments) ERR 403/
+ * Creates a new assessment into the system
+ *
+ * Program participants should be able to create a new
+ * submission for an assessment if:
+ * - they have not reached the limit of the allowed number
+ * of submissions
+ * - the available_after date has passed
+ * - the due_date has not passed
+ *
+ */
+
+/**
+ * (DELETE /assessments/:id) ERR 403/
+ * “Deletes” an assessment in the system
+ *
+ *
+ */
+
+/**
+ * (PUT /assessments/:id) ERR 403/
+ * Edits an assessment in the system
+ *
+ * Program participants should be able to update their
+ * assessment submission if:
+ * - they haven’t clicked the submit button
+ * -  time has not expired
+ * -  due date has not passed
+ *
+ */
+
+/**
+ * (PUT /assessments/:id) Get the meta information about the
+ * assessment (title, list of submission ids, due date, etc)/
+ * Get the meta information about the assessment (title, list
+ * of mentees that have submissions and their submission
+ * ids, due date, etc)
+ *
+ *
+ */
+
+/**
+ * (PUT /assessments/:id/submission/:id) Submits their answers
+ * for this submission/
+ * Submits comments for the submission.
+ *
+ *
+ */
+
+/** (GET /assessments/:id/submission/new) Creates a new (draft)
+ * submission (which starts the timer) and returns the questions
+ * and possible answers and the submission ID number/
+ * Creates a new (draft) submission (which starts the timer) and
+ * returns the questions and possible answers
+ *
+ */
