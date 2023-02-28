4fda093142e44c1d8ca05a87e58d37a00731467f
Nataliia Sokolova <natalia.sokolova.ca@gmail.com>
2023-02-28 12:54:46 -0800

    #515 changed assessmentRouter

--- api/src/middleware/assessmentsRouter.ts
+++ api/src/middleware/assessmentsRouter.ts
@@ -1,24 +1,62 @@
 import { Router } from 'express';
 import { itemEnvelope, collectionEnvelope } from './responseEnvelope';
-import { listAssessments } from '../services/assessmentService';
+import {listAssessmentsByParticipant} from '../services/assessmentService'
 
 const assessmentsRouter = Router();
 
 assessmentsRouter.get('/', async (req, res, next) => {
+  const { principalId } = req.session;
   let assessments;
   try {
-    assessments = await listAssessments();
+    assessments = await listAssessmentsByParticipant(principalId);
   } catch (error) {
     next(error);
     return;
   }
-  res.json(collectionEnvelope(assessments, assessments.length));
+  res.json(
+    collectionEnvelope(assessments, assessments.length)
+  );
 });
 
 assessmentsRouter.post('/', (req, res) => {
   const response = { behaviour: 'Creates a new assessment' };
   res.status(200).json(itemEnvelope(response));
 });
+
+
+
+// assessmentsRouter.post('/', async (req, res, next) => {
+//   const { principalId } = req.session;
+//   const { raw_text: rawText, selected_option_ids: selectedOptionIds } =
+//     req.body;
+//   const optionIds = Array.isArray(selectedOptionIds) ? selectedOptionIds : [];
+//   if (!rawText && !optionIds.length) {
+//     next(
+//       new BadRequestError(
+//         'At least one option id or journal entry text must be given to create a reflection.'
+//       )
+//     );
+//     return;
+//   }
+//   if (optionIds.some(optionId => !Number.isInteger(optionId) || optionId < 1)) {
+//     next(
+//       new ValidationError(
+//         `selected_option_ids must be an array of positive integers.`
+//       )
+//     );
+//     return;
+//   }
+//   let reflection;
+//   try {
+//     reflection = await createReflection(rawText, optionIds, principalId);
+//   } catch (error) {
+//     next(error);
+//     return;
+//   }
+//   res.status(201).json(itemEnvelope(reflection));
+// });
+
+
 
 assessmentsRouter.get('/:assessmentId', (req, res) => {
   const response = { behaviour: 'Shows a single assessment' };
