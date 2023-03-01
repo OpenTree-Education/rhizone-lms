645d03aad73571830c13b765577515f401f22c43
Spencer W <4006420+seidior@users.noreply.github.com>
2023-03-01 13:02:43 -0800

    #516 Assistance with debugging functionality

--- api/src/middleware/assessmentsDummyRouter.ts
+++ api/src/middleware/assessmentsDummyRouter.ts
@@ -9,6 +9,13 @@ import {
 } from '../services/assessmentsDummyService';
 const assessmentsDummyRouter = Router();
 
+interface ProgramParticipantsRow {
+  id: number;
+  principal_id: number;
+  program_id: number;
+  role_id: number;
+}
+
 assessmentsDummyRouter.get(
   '/makeParticipant/:programId/:participantId',
   async (req, res, next) => {
@@ -19,9 +26,9 @@ assessmentsDummyRouter.get(
     const programIdParsed = Number(programId);
     const participantIdParsed = Number(participantId);
 
-    let programParticipantRowId;
+    let insertedProgramParticipantsRow: ProgramParticipantsRow[];
     try {
-      programParticipantRowId = await insertToProgramParticipants(
+      insertedProgramParticipantsRow = await insertToProgramParticipants(
         participantIdParsed,
         programIdParsed,
         roleId
@@ -30,43 +37,73 @@ assessmentsDummyRouter.get(
       next(error);
       return;
     }
-    res.json(
-      itemEnvelope({
-        participantIdParsed,
-        programIdParsed,
-        roleId,
-      })
-    );
+
+    res.status(201).json(itemEnvelope(insertedProgramParticipantsRow));
   }
 );
+
+interface AssessmentSubmissionRow {
+  id: number;
+  assessment_id: number;
+  principal_id: number;
+}
 
 assessmentsDummyRouter.get(
   '/submitAssessment/:participantId',
   async (req, res, next) => {
     const { participantId } = req.params;
-    const { status, submittedAt } = req.body;
-    const assessmentId = 1;
     const participantIdParsed = Number(participantId);
-    const assessmentIdParased = Number(assessmentId);
-    let submitAssessmentParticipantId;
 
+    const dummyAssessmentSubmissionData = {
+      assessmentId: 1,
+      principalId: participantIdParsed,
+      assessmentSubmissionStateId: 7, // graded
+      score: 10,
+      openedAt: '2023-02-09 12:00:00',
+      submittedAt: '2023-02-09 13:23:45',
+      questions: [
+        { id: 1, answerId: 4 },
+        { id: 2, answerId: 5 },
+        { id: 3, answerId: 9 },
+        { id: 4, answerId: 14 },
+        { id: 5, answerId: 18 },
+        { id: 6, answerId: 21 },
+        { id: 7, answerId: 23 },
+        {
+          id: 8,
+          responseText:
+            'const HelloWorld = () => { return <p>Hello, World!</p>; }; export default HelloWorld;',
+        },
+        {
+          id: 9,
+          responseText:
+            'React differs from other JavaScript frameworks because it uses a component-based architecture, a virtual DOM, JSX syntax, unidirectional data flow, and is primarily focused on building UIs rather than providing a complete application framework. These features make it faster, more efficient, and more flexible than other frameworks.',
+        },
+        {
+          id: 10,
+          responseText:
+            'Benefits of using React include its modular and reusable components, efficient updates with virtual DOM, JSX syntax, and active community.',
+        },
+      ],
+    };
+
+    let insertedAssessmentSubmissionsRow: AssessmentSubmissionRow[];
     try {
-      submitAssessmentParticipantId = await insertToAssessmentSubmissions(
-        assessmentIdParased,
-        participantIdParsed,
-        status,
-        submittedAt
+      insertedAssessmentSubmissionsRow = await insertToAssessmentSubmissions(
+        dummyAssessmentSubmissionData.assessmentId,
+        dummyAssessmentSubmissionData.principalId,
+        dummyAssessmentSubmissionData.assessmentSubmissionStateId,
+        dummyAssessmentSubmissionData.score,
+        dummyAssessmentSubmissionData.openedAt,
+        dummyAssessmentSubmissionData.submittedAt,
+        dummyAssessmentSubmissionData.questions
       );
     } catch (error) {
       next(error);
       return;
     }
-    res.json(
-      itemEnvelope({
-        assessmentIdParased,
-        participantIdParsed,
-      })
-    );
+
+    res.status(201).json(itemEnvelope(insertedAssessmentSubmissionsRow));
   }
 );
 
@@ -99,17 +136,17 @@ assessmentsDummyRouter.get(
     );
   }
 );
-assessmentsDummyRouter.get(
-  '/startAssessment/:assessmentId/:participantId',
-  async (req, res, next) => {
-    let assessment;
-    try {
-    } catch (error) {
-      next(error);
-      return;
-    }
-    res.json();
-  }
-);
+// assessmentsDummyRouter.get(
+//   '/startAssessment/:assessmentId/:participantId',
+//   async (req, res, next) => {
+//     let assessment;
+//     try {
+//     } catch (error) {
+//       next(error);
+//       return;
+//     }
+//     res.json();
+//   }
+// );
 
 export default assessmentsDummyRouter;
