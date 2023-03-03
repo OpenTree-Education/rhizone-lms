1e4732ee103cb9f7954ab338c42d8cd54ba030b7
mays4 <maysalani84@yahoo.com>
2023-03-03 16:52:09 -0500

    #516 adding testing for route make facilitator

--- api/src/middleware/__tests__/assessmentsDummyRouter.ts
+++ api/src/middleware/__tests__/assessmentsDummyRouter.ts
@@ -44,31 +44,31 @@ describe('assessmentsDummyRouter', () => {
     });
   });
 
-  // describe('GET /makeFacilitator/:programId/:participantId', () => {
-  //   it('should return program id and  principal Id for program_participants', done => {
-  //     const programId = 2;
-  //     const principalId = 3;
-  //     const roleId = 2;
-  //     const participantRow = [
-  //       {
-  //         id: 2,
-  //         principal_id: principalId,
-  //         program_id: programId,
-  //         role_id: roleId,
-  //       },
-  //     ];
-  //     mockPrincipalId(principalId);
-  //     mockGetInsertToProgramParticipants.mockResolvedValue(participantRow);
-  //     appAgent
-  //       .get(`/makeParticipant/${programId}/${principalId}`)
-  //       .expect(201, itemEnvelope(participantRow), err => {
-  //         expect(mockGetInsertToProgramParticipants).toHaveBeenCalledWith(
-  //           principalId,
-  //           programId,
-  //           roleId
-  //         );
-  //         done(err);
-  //       });
-  //   });
-  // });
+  describe('GET /makeFacilitator/:programId/:participantId', () => {
+    it('should return program id and  principal Id for program_participants', done => {
+      const programId = 2;
+      const principalId = 3;
+      const roleId = 2;
+      const participantRow = [
+        {
+          id: 1,
+          principal_id: principalId,
+          program_id: programId,
+          role_id: roleId,
+        },
+      ];
+      mockPrincipalId(principalId);
+      mockGetInsertToProgramParticipants.mockResolvedValue(participantRow[0]);
+      appAgent
+        .get(`/makeFacilitator/${programId}/${principalId}`)
+        .expect(201, itemEnvelope(participantRow[0]), err => {
+          expect(mockGetInsertToProgramParticipants).toHaveBeenCalledWith(
+            principalId,
+            programId,
+            roleId
+          );
+          done(err);
+        });
+    });
+  });
 });
