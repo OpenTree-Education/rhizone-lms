9632ec0bf1a3dd1b44999762fc08b566af97ae83
Nataliia Sokolova <natalia.sokolova.ca@gmail.com>
2023-03-21 09:49:21 -0700

    #551 added changes to router functions, fixed mock function in tests

--- api/src/middleware/assessmentsRouter.ts
+++ api/src/middleware/assessmentsRouter.ts
@@ -1,13 +1,13 @@
 import { Router } from 'express';
 import { itemEnvelope, collectionEnvelope } from './responseEnvelope';
-import { BadRequestError, ValidationError } from './httpErrors';
+// import { BadRequestError, ValidationError } from './httpErrors';
 import {
-  createAssessment,
-  updateAssessmentById,
-  deleteAssessmentById,
+  // createAssessment,
+  // updateAssessmentById,
+  // deleteAssessmentById,
   getCurriculumAssessmentById,
   // getAssessmentsSummary,
-  getCurriculumAssessmentBasedOnRole,
+  // getCurriculumAssessmentBasedOnRole,
   findRoleInProgram,
   getAssessmentsForProgram,
   principalEnrolledPrograms,
@@ -31,7 +31,7 @@ assessmentsRouter.get('/', async (req, res, next) => {
     const programIds = await principalEnrolledPrograms(principalId);
     const emptyAssessmentsSummaryList: [] = [];
 
-    if (programIds.length === 0) {
+    if (programIds.length === 0 || principalId !== principalId) {
       res.json(collectionEnvelope(emptyAssessmentsSummaryList, 0));
     } else {
       const participantAssessmentListResponse = [];
@@ -54,7 +54,7 @@ assessmentsRouter.get('/', async (req, res, next) => {
           if (roleInProgram.title === 'participant') {
             assessmentSubmissionsSummary =
               await getAssessmentSubmissionsSummary(
-                programAssessment.id,
+                programAssessment.assessment_id,
                 principalId
               );
 
@@ -68,8 +68,8 @@ assessmentsRouter.get('/', async (req, res, next) => {
           if (roleInProgram.title === 'facilitator') {
             facilitatorAssessmentSubmissionsSummary =
               await getFacilitatorAssessmentSubmissionsSummary(
-                programAssessment.id,
-                programId
+                programAssessment.assessment_id,
+                principalId
               );
 
             facilitatorAssessmentListResponse.push({
@@ -121,61 +121,61 @@ assessmentsRouter.get('/', async (req, res, next) => {
 // Creates a new assessment
 // Incoming: CurriculumAssessment (including 'questions') and ProgramAssessment
 // Outgoing: ids of rows inserted into both curriculum_assessments and program_assessments tables
-assessmentsRouter.post('/', async (req, res, next) => {
-  const {
-    title,
-    description,
-    max_score: maxScore,
-    max_num_submissions: maxNumSubmissions,
-    time_limit: timeLimit,
-    available_after: availableAfter,
-    due_date: dueDate,
-    program_id: programId,
-  } = req.body;
-  const { principalId } = req.session;
-  const { curriculum_id: curriculumId, activity_id: activityId } = req.body;
+// assessmentsRouter.post('/', async (req, res, next) => {
+//   const {
+//     title,
+//     description,
+//     max_score: maxScore,
+//     max_num_submissions: maxNumSubmissions,
+//     time_limit: timeLimit,
+//     available_after: availableAfter,
+//     due_date: dueDate,
+//     program_id: programId,
+//   } = req.body;
+//   const { principalId } = req.session;
+//   const { curriculum_id: curriculumId, activity_id: activityId } = req.body;
 
-  if (typeof title !== 'string') {
-    next(new ValidationError('title must be a string!'));
-    return;
-  }
-  if (typeof description !== 'string') {
-    next(new ValidationError('description must be a string!'));
-    return;
-  }
-  if (typeof maxScore !== 'number') {
-    next(new ValidationError('maxScore must be a number!'));
-  }
-  if (typeof maxNumSubmissions !== 'number') {
-    next(new ValidationError('maxNumSubmissions must be a number!'));
-  }
-  if (typeof timeLimit !== 'number') {
-    next(new ValidationError('timeLimit must be a number!'));
-  }
-  if (typeof curriculumId !== 'number') {
-    next(new ValidationError('curriculumId must be a number!'));
-  }
-  let assessment;
-  try {
-    assessment = await createAssessment(
-      title,
-      description,
-      maxScore,
-      maxNumSubmissions,
-      timeLimit,
-      curriculumId,
-      activityId,
-      principalId,
-      availableAfter,
-      programId,
-      dueDate
-    );
-  } catch (error) {
-    next(error);
-    return;
-  }
-  res.status(200).json(itemEnvelope(assessment));
-});
+//   if (typeof title !== 'string') {
+//     next(new ValidationError('title must be a string!'));
+//     return;
+//   }
+//   if (typeof description !== 'string') {
+//     next(new ValidationError('description must be a string!'));
+//     return;
+//   }
+//   if (typeof maxScore !== 'number') {
+//     next(new ValidationError('maxScore must be a number!'));
+//   }
+//   if (typeof maxNumSubmissions !== 'number') {
+//     next(new ValidationError('maxNumSubmissions must be a number!'));
+//   }
+//   if (typeof timeLimit !== 'number') {
+//     next(new ValidationError('timeLimit must be a number!'));
+//   }
+//   if (typeof curriculumId !== 'number') {
+//     next(new ValidationError('curriculumId must be a number!'));
+//   }
+//   let assessment;
+//   try {
+//     assessment = await createAssessment(
+//       title,
+//       description,
+//       maxScore,
+//       maxNumSubmissions,
+//       timeLimit,
+//       curriculumId,
+//       activityId,
+//       principalId,
+//       availableAfter,
+//       programId,
+//       dueDate
+//     );
+//   } catch (error) {
+//     next(error);
+//     return;
+//   }
+//   res.status(200).json(itemEnvelope(assessment));
+// });
 
 //Shows a single assessment
 
@@ -205,100 +205,100 @@ assessmentsRouter.post('/', async (req, res, next) => {
 
 // Incoming: CurriculumAssessment (including 'questions') and ProgramAssessment
 // Outgoing: ids of rows inserted into both curriculum_assessments and program_assessments tables
-assessmentsRouter.put('/assessment/:assessmentId', async (req, res, next) => {
-  const { assessmentId } = req.params;
-  const assessmentIdNumber = Number(assessmentId);
-  const { principalId } = req.session;
-  const {
-    title,
-    description,
-    maxScore,
-    maxNumSubmissions,
-    timeLimit,
-    curiculumId,
-    activityId,
-    questions,
-    availableAfter,
-    dueDate,
-    programId,
-  } = req.body;
+// assessmentsRouter.put('/assessment/:assessmentId', async (req, res, next) => {
+//   const { assessmentId } = req.params;
+//   const assessmentIdNumber = Number(assessmentId);
+//   const { principalId } = req.session;
+//   const {
+//     title,
+//     description,
+//     maxScore,
+//     maxNumSubmissions,
+//     timeLimit,
+//     curiculumId,
+//     activityId,
+//     questions,
+//     availableAfter,
+//     dueDate,
+//     programId,
+//   } = req.body;
 
-  // waiting to figure out how to refactor this into the function outside
-  if (!Number.isInteger(assessmentIdNumber) || assessmentIdNumber < 1) {
-    next(
-      new BadRequestError(`"${assessmentId}" is not a valid assessment id.`)
-    );
-    return;
-  }
+//   // waiting to figure out how to refactor this into the function outside
+//   if (!Number.isInteger(assessmentIdNumber) || assessmentIdNumber < 1) {
+//     next(
+//       new BadRequestError(`"${assessmentId}" is not a valid assessment id.`)
+//     );
+//     return;
+//   }
 
-  // autorizedCheck(principalId, programId, assessmentIdNumber, next);
+//   // autorizedCheck(principalId, programId, assessmentIdNumber, next);
 
-  if (typeof title !== 'string') {
-    next(new ValidationError('title must be a string!'));
-    return;
-  }
-  if (typeof description !== 'string') {
-    next(new ValidationError('description must be a string!'));
-    return;
-  }
-  if (typeof maxScore !== 'number') {
-    next(new ValidationError('maxScore must be a number!'));
-  }
-  if (typeof maxNumSubmissions !== 'number') {
-    next(new ValidationError('maxNumSubmissions must be a number!'));
-  }
-  if (typeof timeLimit !== 'number') {
-    next(new ValidationError('timeLimit must be a number!'));
-  }
-  if (typeof availableAfter !== 'string') {
-    next(new ValidationError('availableAfter must be a string!'));
-  }
-  if (typeof dueDate !== 'string') {
-    next(new ValidationError('dueDate must be a string!'));
-  }
-  try {
-    await updateAssessmentById(
-      title,
-      description,
-      maxScore,
-      maxNumSubmissions,
-      timeLimit,
-      curiculumId,
-      principalId,
-      activityId,
-      questions,
-      availableAfter,
-      dueDate,
-      programId,
-      assessmentIdNumber
-    );
-  } catch (error) {
-    next(error);
-    return;
-  }
-  res.json(itemEnvelope({ id: assessmentId }));
-});
+//   if (typeof title !== 'string') {
+//     next(new ValidationError('title must be a string!'));
+//     return;
+//   }
+//   if (typeof description !== 'string') {
+//     next(new ValidationError('description must be a string!'));
+//     return;
+//   }
+//   if (typeof maxScore !== 'number') {
+//     next(new ValidationError('maxScore must be a number!'));
+//   }
+//   if (typeof maxNumSubmissions !== 'number') {
+//     next(new ValidationError('maxNumSubmissions must be a number!'));
+//   }
+//   if (typeof timeLimit !== 'number') {
+//     next(new ValidationError('timeLimit must be a number!'));
+//   }
+//   if (typeof availableAfter !== 'string') {
+//     next(new ValidationError('availableAfter must be a string!'));
+//   }
+//   if (typeof dueDate !== 'string') {
+//     next(new ValidationError('dueDate must be a string!'));
+//   }
+//   try {
+//     await updateAssessmentById(
+//       title,
+//       description,
+//       maxScore,
+//       maxNumSubmissions,
+//       timeLimit,
+//       curiculumId,
+//       principalId,
+//       activityId,
+//       questions,
+//       availableAfter,
+//       dueDate,
+//       programId,
+//       assessmentIdNumber
+//     );
+//   } catch (error) {
+//     next(error);
+//     return;
+//   }
+//   res.json(itemEnvelope({ id: assessmentId }));
+// });
 
 //TODO: logic should be changed to facilitator/participant role based on permission
 // Deletes” an assessment in the system
-assessmentsRouter.delete('/:assessmentId', async (req, res, next) => {
-  const { assessmentId } = req.params;
-  const assessmentIdNum = Number(assessmentId);
+// assessmentsRouter.delete('/:assessmentId', async (req, res, next) => {
+//   const { assessmentId } = req.params;
+//   const assessmentIdNum = Number(assessmentId);
 
-  if (!Number.isInteger(assessmentIdNum) || assessmentIdNum < 1) {
-    next(
-      new BadRequestError(`"${assessmentIdNum}" is not a valid assessment id.`)
-    );
-    return;
-  }
-  try {
-    await deleteAssessmentById(assessmentIdNum);
-  } catch (error) {
-    next(error);
-    return;
-  }
-  res.status(204).send();
-});
+//   if (!Number.isInteger(assessmentIdNum) || assessmentIdNum < 1) {
+//     next(
+//       new BadRequestError(`"${assessmentIdNum}" is not a valid assessment id.`)
+//     );
+//     return;
+//   }
+//   try {
+//     await deleteAssessmentById(assessmentIdNum);
+//   } catch (error) {
+//     next(error);
+//     return;
+//   }
+//   res.status(204).send();
+// });
 //TODO: fixed the router
 // Outgoing:
 // - participants: CurriculumAssessment (with 'questions' and 'answers' but not the correct answers), ProgramAssessment, and a newly-created AssessmentSubmission
@@ -329,42 +329,42 @@ assessmentsRouter.delete('/:assessmentId', async (req, res, next) => {
 // Outgoing:
 // - participants: CurriculumAssessment (with 'questions' and 'answers' and correct answers (if graded)), ProgramAssessment, and AssessmentSubmission (with 'responses')
 // - facilitator: CurriculumAssessment (with 'questions' and 'answers' and correct answers), ProgramAssessment, and AssessmentSubmission (with 'responses')
-assessmentsRouter.get(
-  '/:assessmentId/submissions/:submissionId',
-  async (req, res, next) => {
-    const { principalId } = req.session;
-    const { assessmentId, submissionId } = req.params;
-    const assessmentIdPrased = Number(assessmentId);
-    const submissionIdPrased = Number(submissionId);
-    if (!Number.isInteger(assessmentIdPrased) || assessmentIdPrased < 1) {
-      next(
-        new BadRequestError(
-          `"${assessmentIdPrased}" is not a valid participant id.`
-        )
-      );
-    }
-    if (!Number.isInteger(submissionIdPrased) || submissionIdPrased < 1) {
-      next(
-        new BadRequestError(
-          `"${submissionIdPrased}" is not a valid participant id.`
-        )
-      );
-    }
-    let listOfAssessmentWithAnswers;
-    try {
-      listOfAssessmentWithAnswers = await getCurriculumAssessmentBasedOnRole(
-        principalId,
-        assessmentIdPrased,
-        submissionIdPrased
-      );
-    } catch (error) {
-      next(error);
-      return;
-    }
-    res.json(itemEnvelope(listOfAssessmentWithAnswers));
-    // res.json(collectionEnvelope(listOfAssessmentWithAnswers,listOfAssessmentWithAnswers.length));
-  }
-);
+// assessmentsRouter.get(
+//   '/:assessmentId/submissions/:submissionId',
+//   async (req, res, next) => {
+//     const { principalId } = req.session;
+//     const { assessmentId, submissionId } = req.params;
+//     const assessmentIdPrased = Number(assessmentId);
+//     const submissionIdPrased = Number(submissionId);
+//     if (!Number.isInteger(assessmentIdPrased) || assessmentIdPrased < 1) {
+//       next(
+//         new BadRequestError(
+//           `"${assessmentIdPrased}" is not a valid participant id.`
+//         )
+//       );
+//     }
+//     if (!Number.isInteger(submissionIdPrased) || submissionIdPrased < 1) {
+//       next(
+//         new BadRequestError(
+//           `"${submissionIdPrased}" is not a valid participant id.`
+//         )
+//       );
+//     }
+//     let listOfAssessmentWithAnswers;
+//     try {
+//       listOfAssessmentWithAnswers = await getCurriculumAssessmentBasedOnRole(
+//         principalId,
+//         assessmentIdPrased,
+//         submissionIdPrased
+//       );
+//     } catch (error) {
+//       next(error);
+//       return;
+//     }
+//     res.json(itemEnvelope(listOfAssessmentWithAnswers));
+//     // res.json(collectionEnvelope(listOfAssessmentWithAnswers,listOfAssessmentWithAnswers.length));
+//   }
+// );
 
 // Incoming: AssessmentSubmission (with 'responses') (but participants shouldn't be allowed to update their scores or update their answers after it's been submitt)
 // Outgoing: AssessmentSubmission (with 'responses')
