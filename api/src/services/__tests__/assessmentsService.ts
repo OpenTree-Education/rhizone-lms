ae4d06aa388bea0a793b9284f8807cd5770ab528
Spencer W <4006420+seidior@users.noreply.github.com>
2023-03-23 18:50:36 -0700

    #515 Service file tests

---
+++ api/src/services/__tests__/assessmentsService.ts
@@ -0,0 +1,31 @@
+describe('assessmentsService', () => {
+  describe('constructFacilitatorAssessmentSummary', () => {});
+
+  describe('constructParticipantAssessmentSummary', () => {});
+
+  describe('createAssessment', () => {});
+
+  describe('createAssessmentSubmission', () => {});
+
+  describe('deleteCurriculumAssessment', () => {});
+
+  describe('deleteProgramAssessment', () => {});
+
+  describe('findProgramAssessment', () => {});
+
+  describe('getAssessmentSubmission', () => {});
+
+  describe('getCurriculumAssessment', () => {});
+
+  describe('getPrincipalProgramRole', () => {});
+
+  describe('listParticipantProgramAssessmentSubmissions', () => {});
+
+  describe('listPrincipalEnrolledProgramIds', () => {});
+
+  describe('listProgramAssessments', () => {});
+
+  describe('updateAssessment', () => {});
+
+  describe('updateAssessmentSubmission', () => {});
+});
