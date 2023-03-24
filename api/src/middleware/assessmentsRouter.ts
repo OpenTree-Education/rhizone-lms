48af82e395cb12b0794cf51ed21259f2cef19de5
Spencer W <4006420+seidior@users.noreply.github.com>
2023-03-23 18:40:12 -0700

    #515 Router file function header corrections
    Missed some `async` keywords.

--- api/src/middleware/assessmentsRouter.ts
+++ api/src/middleware/assessmentsRouter.ts
@@ -3,32 +3,32 @@ import { Router } from 'express';
 const assessmentsRouter = Router();
 
 // List all AssessmentWithSummary to which the user has access
-assessmentsRouter.get('/', async (req, res, next) => { res.json() });
+assessmentsRouter.get('/', async (req, res, next) => { res.json();});
 
 // Get details of a specific CurriculumAssessment
-assessmentsRouter.get('/curriculum/:curriculumAssessmentId', async (req, res, next) => { res.json() });
+assessmentsRouter.get('/curriculum/:curriculumAssessmentId', async (req, res, next) => { res.json();});
 // Create a new CurriculumAssessment
-assessmentsRouter.post('/curriculum', async (req, res, next) => { res.json() });
+assessmentsRouter.post('/curriculum', async (req, res, next) => { res.json(); });
 // Update an existing CurriculumAssessment
-assessmentsRouter.put('/curriculum/:curriculumAssessmentId', async (req, res, next) => { res.json() });
+assessmentsRouter.put('/curriculum/:curriculumAssessmentId', async (req, res, next) => { res.json();});
 // Delete an existing CurriculumAssessment
-assessmentsRouter.delete('/curriculum/:curriculumAssessmentId', async (req, res, next) => { res.json() });
+assessmentsRouter.delete('/curriculum/:curriculumAssessmentId', async (req, res, next) => { res.json();});
 
 // Get details of a specific AssessmentWithSubmissions
-assessmentsRouter.get('/program/:programAssessmentId', async (req, res, next) => { res.json() });
+assessmentsRouter.get('/program/:programAssessmentId', async (req, res, next) => { res.json();});
 // Create a new ProgramAssessment
-assessmentsRouter.post('/program', async (req, res, next) => { res.json() });
+assessmentsRouter.post('/program', async (req, res, next) => { res.json();});
 // Update an existing ProgramAssessment
-assessmentsRouter.put('/program/:programAssessmentId', async (req, res, next) => { res.json() });
+assessmentsRouter.put('/program/:programAssessmentId', async (req, res, next) => { res.json();});
 // Delete an existing ProgramAssessment
-assessmentsRouter.delete('/program/:programAssessmentId', async (req, res, next) => { res.json() });
+assessmentsRouter.delete('/program/:programAssessmentId', async (req, res, next) => { res.json();});
 
 // Start a new AssessmentSubmission
-assessmentsRouter.get('/program/:programAssessmentId/newSubmission', (req, res, next) => { res.json() });
+assessmentsRouter.get('/program/:programAssessmentId/newSubmission', async (req, res, next) => { res.json();});
 
 // Get details of a specific AssessmentSubmission
-assessmentsRouter.get('/submissions/:submissionId', (req, res, next) => { res.json() });
+assessmentsRouter.get('/submissions/:submissionId', async (req, res, next) => { res.json();});
 // Update details of a specific AssessmentSubmission
-assessmentsRouter.put('/submissions/:submissionId', (req, res, next) => { res.json() });
+assessmentsRouter.put('/submissions/:submissionId', async (req, res, next) => { res.json();});
 
 export default assessmentsRouter;
